bfda9d215e70f1d0add6f19a4618d157fbc37d74
Ryan Bolger <z97y9drumh@snkmail.com>
2018-02-07 11:45:46 -0800

    added New-Jws and tests in place of ConvertTo-Jws and Get-JwsSignature

--- Posh-ACME/Private/New-Jws.ps1
+++ Posh-ACME/Private/New-Jws.ps1
@@ -8,7 +8,7 @@ function New-Jws {
         [Parameter(Mandatory,Position=1)]
         [hashtable]$Header,
         [Parameter(Mandatory,Position=2)]
-        [string]$Payload
+        [string]$PayloadJson
     )
 
     # RFC 7515 - JSON Web Signature (JWS)
@@ -22,33 +22,72 @@ function New-Jws {
     # It is assumed the caller has built the header properly for the call
     # being made.
 
-    # Validate the header
+    # validate the header
     if ('alg' -notin $Header.Keys -or $Header.alg -notin 'RS256','EC256') {
         throw "Missing or invalid 'alg' in supplied Header"
     }
-    if ('jwk' -in $Header.Keys -xor 'kid' -in $Header.Keys) {
+    if (!('jwk' -in $Header.Keys -xor 'kid' -in $Header.Keys)) {
         if ('jwk' -in $Header.Keys) {
             throw "Conflicting key entries. Both 'jwk' and 'kid' found in supplied Header"
         } else {
             throw "Missing key entries. Neither 'jwk' or 'kid' found in supplied Header"
         }
     }
+    if ('jwk' -in $Header.Keys -and [string]::IsNullOrWhiteSpace($Header.jwk)) {
+        throw "Empty 'jwk' in supplied Header."
+    }
+    if ('kid' -in $Header.Keys -and [string]::IsNullOrWhiteSpace($Header.kid)) {
+        throw "Empty 'kid' in supplied Header."
+    }
+    if ('nonce' -notin $Header.Keys -or [string]::IsNullOrWhiteSpace($Header.nonce)) {
+        throw "Missing or empty 'nonce' in supplied Header."
+    }
+    if ('url' -notin $Header.Keys -or [string]::IsNullOrWhiteSpace($Header.url)) {
+        throw "Missing or empty 'url' in supplied Header."
+    }
 
-    # Make sure header 'alg' matches key type
-    # RSAKey should be RS256, ECKey should be EC256
+    # build the "<protected>.<payload>" string we're going to be signing
+    Write-Verbose "Header: $($Header | ConvertTo-Json)"
+    $HeaderB64 = ConvertTo-Base64Url ($Header | ConvertTo-Json -Compress)
+    Write-Verbose "Payload: $PayloadJson"
+    $PayloadB64 = ConvertTo-Base64Url $PayloadJson
+    $Message = "$HeaderB64.$PayloadB64"
+    $MessageBytes = [Text.Encoding]::ASCII.GetBytes($Message)
+
     switch ($PSCmdlet.ParameterSetName) {
         'RSAKey' {
+            # Make sure header 'alg' matches key type: RSAKey = RS256, ECKey = EC256
+            if ($Header.alg -ne 'RS256') {
+                throw "Supplied key object does not match 'alg' in supplied Header."
+            }
 
+            # create the signature
+            $HashAlgo = [Security.Cryptography.HashAlgorithmName]::SHA256
+            $PaddingType = [Security.Cryptography.RSASignaturePadding]::Pkcs1
+            $SignedBytes = $RSAKey.SignData($MessageBytes, $HashAlgo, $PaddingType)
             break;
         }
         'ECKey' {
+            # Make sure header 'alg' matches key type: RSAKey = RS256, ECKey = EC256
+            if ($Header.alg -ne 'EC256') {
+                throw "Supplied key object does not match 'alg' in supplied Header."
+            }
+
+            # create the signature
+            $SignedBytes = $ECKey.SignData($MessageBytes)
             break;
         }
         default { throw "Unsupported key type" }
     }
 
+    # now put everything together into the final JWS format
+    $jws = [ordered]@{}
+    $jws.payload = $PayloadB64
+    $jws.protected = $HeaderB64
+    $jws.signature = ConvertTo-Base64Url $SignedBytes
 
-
-
+    # and return it
+    Write-Verbose ($jws | ConvertTo-Json)
+    return ($jws | ConvertTo-Json -Compress)
 
 }
