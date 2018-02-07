04f3a26fa7a9b28b63defd65d91f4cc6dd49fa30
Ryan Bolger <z97y9drumh@snkmail.com>
2018-02-06 17:19:22 -0800

    added more default strings

--- Posh-ACME/Posh-ACME.psm1
+++ Posh-ACME/Posh-ACME.psm1
@@ -21,6 +21,9 @@ Export-ModuleMember -Function $Public.Basename
 $script:LE_PROD = 'https://acme-v02.api.letsencrypt.org/'
 $script:LE_STAGE = 'https://acme-staging-v02.api.letsencrypt.org/'
 $script:NextNonce = ''
+$script:UserAgent = "Posh-ACME/0.1 PowerShell/$($PSVersionTable.PSVersion)"
+$script:CommonHeaders = @{'Accept-Language'='en-us,en;q=0.5'}
+$script:ContentType = 'application/jose+json'
 
 # Set the config path based on edition/platform
 if ('PSEdition' -notin $PSVersionTable.Keys -or $PSVersionTable.PSEdition -eq 'Desktop' -or $IsWindows) {
