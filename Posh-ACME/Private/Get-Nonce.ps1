aaf87fbd03e60c85c0504a4afaf3613ad76d0cb6
Ryan Bolger <z97y9drumh@snkmail.com>
2018-02-14 16:47:59 -0800

    Get-Nonce supports calling with no param

--- Posh-ACME/Private/Get-Nonce.ps1
+++ Posh-ACME/Private/Get-Nonce.ps1
@@ -6,8 +6,20 @@ function Get-Nonce {
 
     # https://tools.ietf.org/html/draft-ietf-acme-acme-09#section-7.2
 
+    # if there was no Uri passed in, check if there's a saved one
+    if (!$NewNonceUri) {
+        if (![string]::IsNullOrWhiteSpace($script:NewNonceUri)) {
+            $NewNonceUri = $script:NewNonceUri
+        } else {
+            throw "No newNonce Uri passed in or previously saved."
+        }
+    }
+
     # super basic for now, no error checking
     $response = Invoke-WebRequest $NewNonceUri -Method Head -UserAgent $script:UserAgent -Headers $script:CommonHeaders -EA Stop
+
+    # save the last used Uri
+    $script:NewNonceUri = $NewNonceUri
 
     return $response.Headers.'Replay-Nonce'
 }
