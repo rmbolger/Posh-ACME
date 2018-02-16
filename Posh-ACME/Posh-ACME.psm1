54c382d47cc9bf70dbfe0f528537e6d37eec6277
Ryan Bolger <z97y9drumh@snkmail.com>
2018-02-15 23:01:17 -0800

    added config loading/creation on module import

--- Posh-ACME/Posh-ACME.psm1
+++ Posh-ACME/Posh-ACME.psm1
@@ -31,11 +31,32 @@ $script:ContentType = 'application/jose+json'
 
 # Set the config path based on edition/platform
 if ('PSEdition' -notin $PSVersionTable.Keys -or $PSVersionTable.PSEdition -eq 'Desktop' -or $IsWindows) {
-    $script:ConfigFolder = Join-Path $env:LOCALAPPDATA 'Posh-ACME'
+    $script:ConfigFolder = $env:LOCALAPPDATA
 } elseif ($IsLinux) {
-    $script:ConfigFolder = Join-Path $env:HOME '.config/Posh-ACME'
+    $script:ConfigFolder = Join-Path $env:HOME '.config'
 } elseif ($IsMacOs) {
-    $script:ConfigFolder = Join-Path $env:HOME 'Library/Preferences/Posh-ACME'
+    $script:ConfigFolder = Join-Path $env:HOME 'Library/Preferences'
 } else {
     throw "Unrecognized PowerShell platform"
+}
+$script:ConfigFile = Join-Path $script:ConfigFolder 'posh-acme.json'
+
+# Import the existing config if there is one
+try {
+    $cfg = Get-Content $script:ConfigFile -Encoding UTF8 -EA Stop | ConvertFrom-Json -EA Stop
+    $script:cfg = $cfg
+} catch {
+    # throw a warning if the config file was found but just couldn't be parsed
+    if (Test-Path $script:ConfigFile) {
+        Write-Warning "Config file found but content is invalid. Creating new config."
+        Move-Item $script:ConfigFile "$($script:ConfigFile).bad" -Force
+    }
+
+    # create a new config
+    $script:cfg = [pscustomobject]@{
+        CurrentDir = [string]::Empty
+    }
+
+    # write the config to disk
+    $script:cfg | ConvertTo-Json | Out-File $script:ConfigFile -Encoding UTF8
 }
