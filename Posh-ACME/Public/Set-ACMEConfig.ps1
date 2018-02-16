d7c9368cbf8744add030376be380727c9a90026d
Ryan Bolger <z97y9drumh@snkmail.com>
2018-02-15 23:30:04 -0800

    WIP Set-ACMEConfig

--- Posh-ACME/Public/Set-ACMEConfig.ps1
+++ Posh-ACME/Public/Set-ACMEConfig.ps1
@@ -8,6 +8,7 @@ function Set-ACMEConfig {
         [string]$CustomACMEServer
     )
 
+    $SaveChanges = $false
 
     if ('WellKnownACMEServer' -in $PSBoundParameters.Keys -or 'CustomACMEServer' -in $PSBoundParameters.Keys) {
         # grab the appropriate directory URI
@@ -17,16 +18,32 @@ function Set-ACMEConfig {
             $DirUri = $CustomACMEServer
         }
 
-        # create the config entry for this Uri if it doesn't exist
-        if (!$script:cfg.$DirUri) {
-            $script:cfg.$DirUri = @{
-                Accounts = @{}
-            }
-
+        if ($script:cfg.CurrentDir -ne $DirUri) {
+            $script:cfg.CurrentDir = $DirUri
+            $SaveChanges = $true
         }
 
+        # create the config entry for this Uri if it doesn't exist
+        if (!$script:cfg.$DirUri) {
+            $newcfg = [pscustomobject]@{
+                Accounts = @{}
+            }
+            $script:cfg | Add-Member -MemberType NoteProperty -Name $DirUri -Value $newcfg
+            $SaveChanges = $true
+        }
+
+    } elseif ([string]::IsNullOrWhiteSpace($script:cfg.CurrentDir)) {
+        throw "No ACME Server specified and no saved value found."
     }
 
+    # make a variable shortcut to the current server's config
+    $curcfg = $script:cfg.$script:CurrentDir
 
+
+
+    if ($SaveChanges) {
+        # write the config to disk
+        $script:cfg | ConvertTo-Json | Out-File $script:ConfigFile -Encoding UTF8
+    }
 
 }
