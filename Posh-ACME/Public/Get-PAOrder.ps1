4a94d5f74a4c07d951b3439ce51cb56e4f192c7f
Ryan Bolger <z97y9drumh@snkmail.com>
2018-04-01 23:02:45 -0700

    added the ability to specify an order in Get-PAOrder

--- Posh-ACME/Public/Get-PAOrder.ps1
+++ Posh-ACME/Public/Get-PAOrder.ps1
@@ -1,6 +1,10 @@
 function Get-PAOrder {
+    [OutputType('PoshACME.PAOrder')]
     [CmdletBinding()]
     param(
+        [Parameter(ParameterSetName='Specific',Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string]$MainDomain,
+        [Parameter(ParameterSetName='List',Mandatory)]
         [switch]$List,
         [switch]$Refresh
     )
@@ -10,25 +14,48 @@ function Get-PAOrder {
         throw "No ACME account configured. Run Set-PAAccount first."
     }
 
-    if ($List) {
+    # List mode
+    if ('List' -eq $PSCmdlet.ParameterSetName) {
+
         # read the contents of each order's order.json
-        Get-ChildItem "$($script:AcctFolder)\*\order.json" | Get-Content -Raw |
-            ConvertFrom-Json | Sort-Object MainDomain | ForEach-Object {
+        $rawOrders = Get-ChildItem "$($script:AcctFolder)\*\order.json" | Get-Content -Raw
+        $rawOrders | ConvertFrom-Json | Sort-Object MainDomain | ForEach-Object {
 
-                $_.PSObject.TypeNames.Insert(0,'PoshACME.PAOrder')
+            # insert the type so it displays properly
+            $_.PSObject.TypeNames.Insert(0,'PoshACME.PAOrder')
 
-                # update the data from the server
-                if ($Refresh -and $_.status -ne 'invalid') { Update-PAOrder $_ }
-                Write-Output $_
-            }
+            # update the data from the server
+            if ($Refresh -and $_.status -ne 'invalid') { Update-PAOrder $_ }
+            Write-Output $_
+        }
+
+    # Specific mode
     } else {
 
-        $order = $script:Order
+        if ($MainDomain) {
 
-        # update the data from the server if requested
-        if ($Refresh) { Update-PAOrder $order }
+            # build the path to the order file
+            $domainFolder = $MainDomain.Replace('*','!')
+            $orderFile = "$($script:AcctFolder)\$domainFolder\order.json"
 
-        $script:Order
+            # check for an order.json
+            if (Test-Path $orderFile -PathType Leaf) {
+                $order = Get-ChildItem $orderFile | Get-Content -Raw | ConvertFrom-Json
+                $order.PSObject.TypeNames.Insert(0,'PoshACME.PAOrder')
+            }
+
+        } else {
+            # just use the current one
+            $order = $script:Order
+        }
+
+        if ($order) {
+            # update the data from the server if requested
+            if ($Refresh) { Update-PAOrder $order }
+
+            return $order
+        }
+
     }
 
 }
