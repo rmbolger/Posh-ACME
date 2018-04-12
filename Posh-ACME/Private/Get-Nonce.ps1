dbc9418b0cadcc95fe220dcaaa6117c663fed870
Ryan Bolger <z97y9drumh@snkmail.com>
2018-04-12 11:03:26 -0700

    made Get-Nonce a bit more robust

--- Posh-ACME/Private/Get-Nonce.ps1
+++ Posh-ACME/Private/Get-Nonce.ps1
@@ -1,25 +1,36 @@
 function Get-Nonce {
     [CmdletBinding()]
     param(
+        [Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [Alias('newNonce')]
         [string]$NewNonceUrl
     )
 
     # https://tools.ietf.org/html/draft-ietf-acme-acme-10#section-7.2
 
-    # if there was no Url passed in, check if there's a saved one
-    if (!$NewNonceUrl) {
-        if (![string]::IsNullOrWhiteSpace($script:Dir.newNonce)) {
-            $NewNonceUrl = $script:Dir.newNonce
-        } else {
-            throw "No newNonce Url passed in or previously saved."
+    Process {
+
+        # if there was no Url passed in, check if there's a saved one
+        if (!$NewNonceUrl) {
+            if (!$script:Dir -or !$script:Dir.newNonce) {
+                throw "No NewNonceUrl passed in or saved on current PAServer."
+            } else {
+                $NewNonceUrl = $script:Dir.newNonce
+            }
         }
+
+        # make the request
+        Write-Verbose "Requesting nonce from $NewNonceUrl"
+        try {
+            $response = Invoke-WebRequest $NewNonceUrl -Method Head -UserAgent $script:USER_AGENT -Headers $script:COMMON_HEADERS -EA Stop -Verbose:$false
+        } catch { throw }
+
+        # return the value from the response
+        if ($response.Headers.ContainsKey($script:HEADER_NONCE)) {
+            return $response.Headers.$script:HEADER_NONCE
+        } else {
+            throw "$($script:HEADER_NONCE) not found in response headers."
+        }
+
     }
-
-    # super basic for now, no error checking
-    Write-Verbose "Requesting new nonce from $NewNonceUrl"
-    try {
-        $response = Invoke-WebRequest $NewNonceUrl -Method Head -UserAgent $script:USER_AGENT -Headers $script:COMMON_HEADERS -EA Stop
-    } catch { throw }
-
-    return $response.Headers.$script:HEADER_NONCE
 }
