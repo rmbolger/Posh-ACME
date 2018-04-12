1ccace2716c88623273be6d3a9237b7b0a30389e
Ryan Bolger <z97y9drumh@snkmail.com>
2018-04-11 20:42:34 -0700

    WIP csr and pem generation with BouncyCastle

--- Posh-ACME/Private/New-PACSR.ps1
+++ Posh-ACME/Private/New-PACSR.ps1
@@ -1,3 +1,14 @@
+using namespace Org.BouncyCastle.Asn1
+using namespace Org.BouncyCastle.Asn1.Nist
+using namespace Org.BouncyCastle.Asn1.Pkcs
+using namespace Org.BouncyCastle.Asn1.X509
+using namespace Org.BouncyCastle.Crypto
+using namespace Org.BouncyCastle.Crypto.Generators
+using namespace Org.BouncyCastle.Crypto.Parameters
+using namespace Org.BouncyCastle.Pkcs
+using namespace Org.BouncyCastle.Security
+using namespace Org.BouncyCastle.X509.Extension
+
 function New-PACSR {
     [CmdletBinding()]
     param(
@@ -6,165 +17,98 @@ function New-PACSR {
 
         [Parameter(ParameterSetName='NewKey',Position=1)]
         [ValidateScript({Test-ValidKeyLength $_ -ThrowOnFail})]
-        [string]$KeyLength='4096'
+        [string]$KeyLength='4096',
 
         # [Parameter(ParameterSetName='OldKey',Position=1)]
         # [ValidateScript({Test-ValidKey $_ -ThrowOnFail})]
         # [Security.Cryptography.AsymmetricAlgorithm]$Key
+
+        [switch]$OCSPMustStaple
     )
 
-    # There are a number of ways to go here, none are great, and all of them are very Windows-only
-    # for the time being. We're gonna try to make the legacy COM based CertEnroll APIs work for now
-    # because it ultimately gives us a more control. But if that falls through, we could also just
-    # call out to to certreq.exe with a dynamically created inf file. There's a new
-    # CertificateRequest class in .NET Core 2.0, but since the rest of the module is currently
-    # targetting Windows .NET Framework, that's a no go. Though supposedly it's being added in
-    # .NET 4.7.2 which is currently in preview. But even after release, we may not want to force
-    # people on to the bleeding edge framework just to get an LE cert.
+    # Unfortunately, the .NET managed X509 classes aren't quite there yet in terms of the functionality
+    # we need. There's a new CertificateRequest class in .NET Core 2.0, but it won't be in the full
+    # .NET Framework until 4.7.2 which is still in preview and who knows what the platform requirements
+    # will be when they release. The legacy CertEnroll COM APIs are an option but my first attempt at
+    # at working with them ultimately ended in failure and they felt too tightly coupled to the Windows
+    # cert store. I contemplated just shell'ing out to certreq.exe and certutil.exe, but they're just
+    # not granular enough to do what I want to do.
 
-    # IObjectId::InitializeFromAlgorithmName
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa376796%28v=vs.85%29.aspx
-    $XCN_CRYPT_PUBKEY_ALG_OID_GROUP_ID = 3
-    $XCN_CRYPT_OID_INFO_PUBKEY_ANY = 0
-    $AlgorithmFlagsNone = 0
-
-    # X509KeySpec enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa379409%28v=vs.85%29.aspx
-    $XCN_AT_KEYEXCHANGE = 1
-
-    # X509PrivateKeyUsageFlags enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa379417(v=vs.85).aspx
-    $XCN_NCRYPT_ALLOW_DECRYPT_FLAG = 0x1
-    $XCN_NCRYPT_ALLOW_SIGNING_FLAG = 0x2
-    $XCN_NCRYPT_ALLOW_KEY_AGREEMENT_FLAG = 0x4
-
-    # X509PrivateKeyExportFlags enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa379412(v=vs.85).aspx
-    $XCN_NCRYPT_ALLOW_EXPORT_FLAG = 0x1
-    $XCN_NCRYPT_ALLOW_PLAINTEXT_EXPORT_FLAG = 0x2
-    $XCN_NCRYPT_ALLOW_ARCHIVING_FLAG = 0x4
-    $XCN_NCRYPT_ALLOW_PLAINTEXT_ARCHIVING_FLAG = 0x8
-
-    # X509CertificateEnrollmentContext enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa379399(v=vs.85).aspx
-    $ContextUser = 0x1
-
-    # X500NameFlags enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa379394%28v=vs.85%29.aspx
-    $XCN_CERT_NAME_STR_NONE = 0
-
-    # AlternativeNameType enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa374830%28v=vs.85%29.aspx
-    $XCN_CERT_ALT_NAME_DNS_NAME = 3
-
-    # EncodingType enumeration
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/aa374936(v=vs.85).aspx
-    $XCN_CRYPT_STRING_BASE64 = 0x1
-    $XCN_CRYPT_STRING_NOCRLF = 0x40000000
-
+    # So for now, we're going to leverage the .NET version of the Bouncy Castle libraries. It's a binary
+    # dependency I was hoping to avoid. But it seems like the best option until the native BCL matures.
 
     # create the private key if necessary
     if ('NewKey' -eq $PSCmdlet.ParameterSetName) {
 
-        # Until we figure out how to convert the .NET AsymmetricAlgorithm keys to IX509PrivateKey
-        # objects, we're going to have to create these directly using the COM interfaces
+        # Until we figure out how to convert the .NET AsymmetricAlgorithm keys to BouncyCastle
+        # AsymmetricCipherKeyPair objects, we're going to have to create these directly using
+        # BouncyCastle
+        $sRandom = New-Object SecureRandom
 
-        $isRSA = $true
-        $HashAlgo = 'SHA256'
         if ($KeyLength -like 'ec-*') {
+
             $isRSA = $false
             $keySize = [int]$KeyLength.Substring(3)
+            $curveOid = [NistNamedCurves]::GetOid("P-$keySize")
+
+            if ($keySize -eq 256) { $sigAlgo = 'SHA256WITHECDSA' }
+            elseif ($keySize -eq 384) { $sigAlgo = 'SHA384WITHECDSA' }
+            elseif ($keySize -eq 521) { $sigAlgo = 'SHA512WITHECDSA' }
+
+            $ecGen = New-Object ECKeyPairGenerator
+            $genParam = New-Object ECKeyGenerationParameters -ArgumentList $curveOid,$sRandom
+            $ecGen.Init($genParam)
+            $keyPair = $ecGen.GenerateKeyPair()
+
         } else {
+
+            $isRSA = $true
             $keySize = [int]$KeyLength
+            $sigAlgo = 'SHA256WITHRSA'
+
+            $rsaGen = New-Object RsaKeyPairGenerator
+            $genParam = New-Object KeyGenerationParameters -ArgumentList $sRandom,$keySize
+            $rsaGen.Init($genParam)
+            $keyPair = $rsaGen.GenerateKeyPair()
+
         }
 
-        $algo = New-Object -ComObject X509Enrollment.CObjectId
-        if ($isRSA) {
-            $algo.InitializeFromAlgorithmName($XCN_CRYPT_PUBKEY_ALG_OID_GROUP_ID,$XCN_CRYPT_OID_INFO_PUBKEY_ANY,$AlgorithmFlagsNone,'RSA')
-        } elseif ($keySize -eq 256) {
-            $algo.InitializeFromAlgorithmName($XCN_CRYPT_PUBKEY_ALG_OID_GROUP_ID,$XCN_CRYPT_OID_INFO_PUBKEY_ANY,$AlgorithmFlagsNone,'ECDSA_P256')
-        } elseif ($keySize -eq 384) {
-            $algo.InitializeFromAlgorithmName($XCN_CRYPT_PUBKEY_ALG_OID_GROUP_ID,$XCN_CRYPT_OID_INFO_PUBKEY_ANY,$AlgorithmFlagsNone,'ECDSA_P384')
-            $HashAlgo = 'SHA384'
-        } elseif ($keySize -eq 521) {
-            $algo.InitializeFromAlgorithmName($XCN_CRYPT_PUBKEY_ALG_OID_GROUP_ID,$XCN_CRYPT_OID_INFO_PUBKEY_ANY,$AlgorithmFlagsNone,'ECDSA_P521')
-            $HashAlgo = 'SHA512'
-        }
-
-        # create the private key
-        $privKey = New-Object -ComObject X509Enrollment.CX509PrivateKey -Property @{
-            ProviderName = 'Microsoft Software Key Storage Provider';
-            Algorithm = $algo;
-            Length = $keySize;
-            KeySpec = $XCN_AT_KEYEXCHANGE;
-            KeyUsage = ($XCN_NCRYPT_ALLOW_DECRYPT_FLAG -bor $XCN_NCRYPT_ALLOW_SIGNING_FLAG -bor $XCN_NCRYPT_ALLOW_KEY_AGREEMENT_FLAG);
-            ExportPolicy = ($XCN_NCRYPT_ALLOW_EXPORT_FLAG -bor $XCN_NCRYPT_ALLOW_PLAINTEXT_EXPORT_FLAG -bor $XCN_NCRYPT_ALLOW_ARCHIVING_FLAG -bor $XCN_NCRYPT_ALLOW_PLAINTEXT_ARCHIVING_FLAG);
-            MachineContext = $false;
-        }
-        $privKey.Create()
     }
 
-    # start building the request
-    $req = New-Object -ComObject X509Enrollment.CX509CertificateRequestPkcs10
-    $req.InitializeFromPrivateKey($ContextUser, $privKey, [string]::Empty)
-    $req.SuppressDefaults = $true
+    # create the subject
+    $subject = New-Object X509Name("CN=$($Domain[0])")
 
-    # set the hashing algorithm
-    $HashAlgoId = New-Object -ComObject X509Enrollment.CObjectId
-    $HashAlgoId.InitializeFromValue(([Security.Cryptography.Oid]$HashAlgo).Value)
-    $req.HashAlgorithm = $HashAlgoId
+    # create a .NET Dictionary to hold our extensions because that's what BouncyCastle needs
+    $extDict = New-Object 'Collections.Generic.Dictionary[DerObjectIdentifier,X509Extension]'
 
-    # set the subject
-    $dn = New-Object -ComObject X509Enrollment.CX500DistinguishedName
-    $dn.Encode("CN=$($Domain[0])", $XCN_CERT_NAME_STR_NONE)
-    $req.Subject = $dn
+    # create the extensions we care about
+    $basicConstraints = New-Object X509Extension($false, (New-Object DerOctetString(New-Object BasicConstraints($false))))
+    $keyUsage = New-Object X509Extension($true, (New-Object DerOctetString(New-Object KeyUsage([KeyUsage]::DigitalSignature -bor [KeyUsage]::KeyEncipherment))))
+    $extKeyUsage = New-Object X509Extension($false, (New-Object DerOctetString(New-Object ExtendedKeyUsage([KeyPurposeID]::IdKPServerAuth, [KeyPurposeID]::IdKPClientAuth))))
+    $genNames = @()
+    foreach ($name in $Domain) { $genNames += New-Object GeneralName([GeneralName]::DnsName, $name) }
+    $sans = New-Object X509Extension($false, (New-Object DerOctetString(New-Object GeneralNames(@(,$genNames)))))
+    $ski = New-Object X509Extension($false, (New-Object DerOctetString(New-Object SubjectKeyIdentifierStructure($keyPair.Public))))
 
-    # add the typical KeyUsage flags
-    $keyUsage = New-Object -ComObject X509Enrollment.CX509ExtensionKeyUsage
-    $keyUsage.InitializeEncode([int][Security.Cryptography.X509Certificates.X509KeyUsageFlags](('DigitalSignature','KeyEncipherment')))
-    $keyUsage.Critical = $true
-    $req.X509Extensions.Add($keyUsage)
+    # add them to a DerSet object
+    $extDict.Add([X509Extensions]::BasicConstraints, $basicConstraints)
+    $extDict.Add([X509Extensions]::KeyUsage, $keyUsage)
+    $extDict.Add([X509Extensions]::ExtendedKeyUsage, $extKeyUsage)
+    $extDict.Add([X509Extensions]::SubjectAlternativeName, $sans)
+    $extDict.Add([X509Extensions]::SubjectKeyIdentifier, $ski)
 
-    # add Server/Client Authentication extensions
-    $ekuOids = New-Object -ComObject X509Enrollment.CObjectIds
-    '1.3.6.1.5.5.7.3.1', '1.3.6.1.5.5.7.3.2' | ForEach-Object {
-        $oid = New-Object -ComObject X509Enrollment.CObjectId
-        $oid.InitializeFromValue($_)
-        $ekuOids.Add($oid)
+    # add OCSP Must Staple if requested
+    if ($OCSPMustStaple) {
+        $mustStaple = New-Object X509Extension($false, (New-Object DerOctetString(@(,[byte[]](0x30,0x03,0x02,0x01,0x05)))))
+        $extDict.Add((New-Object DerObjectIdentifier('1.3.6.1.5.5.7.1.24')), $mustStaple)
     }
-    $eku = New-Object -ComObject X509Enrollment.CX509ExtensionEnhancedKeyUsage
-    $eku.InitializeEncode($ekuOids)
-    $req.X509Extensions.Add($eku)
 
-    # add SANs
-    $sans = New-Object -ComObject X509Enrollment.CAlternativeNames
-    $Domain | ForEach-Object {
-        $san = New-Object -ComObject X509Enrollment.CAlternativeName
-        $san.InitializeFromString($XCN_CERT_ALT_NAME_DNS_NAME, $_)
-        $sans.Add($san)
-    }
-    $extSans = New-Object -ComObject X509Enrollment.CX509ExtensionAlternativeNames
-    $extSans.InitializeEncode($sans)
-    $req.X509Extensions.Add($extSans)
+    # build the extensions DerSet
+    $extensions = New-Object X509Extensions($extDict)
+    $extDerSet = New-Object DerSet(New-Object AttributePkcs([PkcsObjectIdentifiers]::Pkcs9AtExtensionRequest,(New-Object DerSet($extensions))))
 
-    # if ($OcspMustStaple) {
-    #     # X509Extension(OID(1.3.6.1.5.5.7.1.24), SEQUENCE(INTEGER(5)))
-    #     $objectId = New-Object -ComObject X509Enrollment.CObjectId
-    #     $objectId.InitializeFromValue("1.3.6.1.5.5.7.1.24")
-    #     $ocsp = New-Object -ComObject X509Enrollment.CX509Extension
-    #     $ocsp.Initialize($objectId, 1, [System.Convert]::ToBase64String((Encode-ASN1Sequence(Encode-ASN1Integer 5))))
-    #     $request.X509Extensions.Add($ocsp)
-    # }
+    # create the request object
+    $req = New-Object Pkcs10CertificationRequest($sigAlgo,$subject,$keyPair.Public,$extDerSet,$keyPair.Private)
 
-    # encode the request
-    $req.Encode()
-
-    # create the enrollment object
-    $enroll = New-Object -ComObject X509Enrollment.CX509Enrollment
-    $enroll.InitializeFromRequest($req)
-    $enroll.CertificateFriendlyName = $Domain[0]
-
-    $csr = $enroll.CreateRequest($XCN_CRYPT_STRING_BASE64 -bor $XCN_CRYPT_STRING_NOCRLF) | ConvertTo-Base64Url -FromBase64
-
-    return $csr
+    $req
 }
