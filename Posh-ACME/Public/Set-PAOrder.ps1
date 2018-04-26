ae3e9b9bf53753b413aef5735d3478e0ae12d4c7
Ryan Bolger <z97y9drumh@snkmail.com>
2018-04-25 17:06:45 -0700

    implemented cert revocation in Set-PAOrder

--- Posh-ACME/Public/Set-PAOrder.ps1
+++ Posh-ACME/Public/Set-PAOrder.ps1
@@ -1,9 +1,11 @@
 function Set-PAOrder {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param(
-        [Parameter(Mandatory,Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [Parameter(Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
         [string]$MainDomain,
-        [switch]$RevokeCert
+        [switch]$RevokeCert,
+        [switch]$NoSwitch,
+        [switch]$Force
     )
 
     Begin {
@@ -15,12 +17,28 @@ function Set-PAOrder {
 
     Process {
 
-        # It's entirely possible that someone could pipe a bunch of domains to this function
-        # and basically switch orders a bunch of times. But ultimately there's no harm in it,
-        # so there's not reason to do anything about it.
+        # throw an error if there's no current order and no MainDomain
+        # passed in
+        if (!$script:Order -and !$MainDomain) {
+            throw "No ACME order configured. Run New-PAOrder or specify a MainDomain."
+        }
 
-        # check if we're switching orders
-        if ($script:Order -and $MainDomain -ne $script:Order.MainDomain) {
+        # There are 3 types of calls the user might be making here.
+        # - order switch
+        # - order switch and revocation
+        # - revocation only (possibly bulk via pipeline)
+        # The default is to switch orders. So we have a -NoSwitch parameter to
+        # indicate a non-switching revocation. But there's a chance they could forget
+        # to use it for a bulk update. For now, we'll just let it happen and switch
+        # to whatever order came through the pipeline last.
+
+        if ($NoSwitch -and $MainDomain) {
+            # This is a non-switching revocation, so grab a cached reference to the
+            # order specified
+            $order = Get-PAOrder $MainDomain
+
+        } elseif (!$script:Order -or ($MainDomain -and ($MainDomain -ne $script:Order.MainDomain))) {
+            # This is a definite order switch
 
             # refresh the cached copy
             Update-PAOrder $MainDomain
@@ -32,6 +50,80 @@ function Set-PAOrder {
 
             # reload the cache from disk
             Import-PAConfig 'Order'
+
+            # grab a local reference to the newly current order
+            $order = $script:Order
+
+        } else {
+            # This is effectively a non-switching revocation because they didn't
+            # specify a MainDomain. So just use the current order.
+            $order = $script:Order
+        }
+
+        # revoke if necessary
+        if ($RevokeCert) {
+
+            # make sure the order is valid
+            if ($order.status -ne 'valid') {
+                Write-Warning "Unable to revoke certificate. Order for $($order.MainDomain) is not valid."
+                return
+            }
+
+            # make sure the cert file actually exists
+            $certFile = Join-Path (Join-Path $script:AcctFolder $order.MainDomain.Replace('*','!')) 'cert.cer'
+            if (!(Test-Path $certFile -PathType Leaf)) {
+                Write-Warning "Unable to revoke certificate. $certFile not found."
+                return
+            }
+
+            # confirm revocation unless -Force was used
+            if (!$Force) {
+                if (!$PSCmdlet.ShouldContinue("Are you sure you wish to revoke $($order.MainDomain)?",
+                "Revoking a certificate is irreversible and will immediately break any services using it.")) {
+                    Write-Verbose "Revocation aborted for $($order.MainDomain)."
+                    return
+                }
+            }
+
+            Write-Verbose "Revoking certificate $($order.MainDomain)."
+
+            # grab the cert file contents, strip the headers, and join the lines
+            $certStart = -1; $certEnd = -1;
+            $certLines = Get-Content $certFile
+            for ($i=0; $i -lt $certLines.Count; $i++) {
+                if ($certLines[$i] -eq '-----BEGIN CERTIFICATE-----') {
+                    $certStart = $i + 1
+                } elseif ($certLines[$i] -eq '-----END CERTIFICATE-----') {
+                    $certEnd = $i - 1
+                    break
+                }
+            }
+            if ($certStart -lt 0 -or $certEnd -lt 0) {
+                throw "Malformed certificate file. $certFile"
+            }
+            $certStr = $certLines[$certStart..$certEnd] -join '' | ConvertTo-Base64Url -FromBase64
+
+            # hydrate the account key
+            $acctKey = $acct.key | ConvertFrom-Jwk
+
+            # build the header
+            $header = @{
+                alg   = $acct.alg;
+                kid   = $acct.location;
+                nonce = $script:Dir.nonce;
+                url   = $script:Dir.revokeCert;
+            }
+
+            $payloadJson = "{`"certificate`":`"$certStr`"}"
+
+            # send the request
+            try {
+                $response = Invoke-ACME $header.url $acctKey $header $payloadJson -EA Stop
+            } catch { throw }
+            Write-Debug "Response: $($response.Content)"
+
+            # refresh the order
+            Update-PAOrder $order.MainDomain
 
         }
 
@@ -46,15 +138,29 @@ function Set-PAOrder {
         Set the current ACME order.
 
     .DESCRIPTION
-        Allows you to switch between ACME orders for a particular account.
+        Switch between ACME orders on an account and/or revoke an order's certificate. Revoked certificate orders are not deleted and can be re-requested Submit-Renewal or New-PACertificate.
 
     .PARAMETER MainDomain
         The primary domain for the order. For a SAN order, this was the first domain in the list when creating the order.
+
+    .PARAMETER RevokeCert
+        If specified, a request will be sent to the associated ACME server to revoke the certificate on this order. Clients may wish to do this if the certificate is decommissioned or the private key has been compromised. A warning will be displayed if the order is not currently valid or the existing certificate file can't be found.
+
+    .PARAMETER NoSwitch
+        If specified, the currently selected order will not change. Useful primarily for bulk certificate revocation. This switch is ignored if no MainDomain is specified.
+
+    .PARAMETER Force
+        If specified, confirmation prompts for certificate revocation will be skipped.
 
     .EXAMPLE
         Set-PAOrder site1.example.com
 
         Switch to the specified domain's order.
+
+    .EXAMPLE
+        Set-PAORder -RevokeCert
+
+        Revoke the current order's certificate.
 
     .LINK
         Project: https://github.com/rmbolger/Posh-ACME
