9f0b9aab72a336b385ea1a65ada435920ca366eb
Ryan Bolger <z97y9drumh@snkmail.com>
2019-03-19 08:23:54 -0700

    more tests for Export-PluginArgs

--- Tests/Export-PluginArgs.Tests.ps1
+++ Tests/Export-PluginArgs.Tests.ps1
@@ -14,6 +14,7 @@ Describe "Export-PluginArgs" {
 
         Mock Get-DirFolder { return 'TestDrive:\' }
         New-Item "TestDrive:\$($fakeAcct.id)" -ItemType Directory -ErrorAction SilentlyContinue
+        $pDataFile = "TestDrive:\$($fakeAcct.id)\plugindata.xml"
 
         Context "No active account" {
 
@@ -42,7 +43,6 @@ Describe "Export-PluginArgs" {
                 R53ProfileName='fake-profile'
                 MyFakeParam='this has no plugin'
             }
-            $pDataFile = "TestDrive:\$($fakeAcct.id)\plugindata.xml"
 
             It "Does not throw" {
                 Remove-Item $pDataFile -Force -EA SilentlyContinue
@@ -73,7 +73,7 @@ Describe "Export-PluginArgs" {
                 R53ProfileName='fake-profile'
                 MyFakeParam='this has no plugin'
             }
-            'Unparseable data for Import-CliXml' | Out-File "TestDrive:\$($fakeAcct.id)\plugindata.xml"
+            'Unparseable data for Import-CliXml' | Out-File $pDataFile
 
             It "Throws with unparseable plugindata.xml" {
                 { Export-PluginArgs $pargs Route53 }          | Should -Throw
@@ -81,22 +81,118 @@ Describe "Export-PluginArgs" {
                 { Export-PluginArgs $pargs FakePlugin }       | Should -Throw
             }
 
-            Remove-Item "TestDrive:\$($fakeAcct.id)\plugindata.xml" -Force
+            Remove-Item $pDataFile -Force
         }
 
         Context "Valid plugindata.xml (no encryption)" {
 
             Mock Get-PAAccount { $fakeAcct }
-            $pargs = @{
-                IBUsername = 'fakeuser'
-                IBPassword = 'fakepass'
-                IBServer = 'fake.example.com'
-                R53ProfileName = 'fake-profile'
-                AliKeyId = 'fakeid'
-                AliSecretInsecure = 'fakesecret'
+
+            It "Replaces old values for 1 plugin" {
+                @{
+                    IBUsername = 'old-user'
+                    IBPassword = 'old-pass'
+                } | Export-CliXml $pDataFile
+                $pargs = @{
+                    IBUsername = 'new-user'
+                    IBPassword = 'new-pass'
+                }
+                Export-PluginArgs $pargs Infoblox
+                $result = Import-CliXml $pDataFile
+
+                $result.Keys.Count | Should -Be 2
+                $result.IBUsername | Should -BeExactly 'new-user'
+                $result.IBPassword | Should -BeExactly 'new-pass'
             }
 
+            It "Replaces old values for multiple plugins" {
+                @{
+                    IBUsername = 'old-user'
+                    IBPassword = 'old-pass'
+                    R53ProfileName = 'old-profile'
+                } | Export-CliXml $pDataFile
+                $pargs = @{
+                    IBUsername = 'new-user'
+                    IBPassword = 'new-pass'
+                    R53ProfileName = 'new-profile'
+                }
+                Export-PluginArgs $pargs Infoblox,Route53
+                $result = Import-CliXml $pDataFile
 
+                $result.Keys.Count | Should -Be 3
+                $result.IBUsername | Should -BeExactly 'new-user'
+                $result.IBPassword | Should -BeExactly 'new-pass'
+                $result.R53ProfileName | Should -BeExactly 'new-profile'
+            }
+
+            It "Removes conflicting parameters for 1 plugin" {
+                @{ R53ProfileName = 'old-profile' } | Export-CliXml $pDataFile
+                $pargs = @{
+                    R53AccessKey = 'new-key'
+                    R53SecretKeyInsecure = 'new-secret'
+                }
+                Export-PluginArgs $pargs Route53
+                $result = Import-Clixml $pDataFile
+
+                $result.Keys.Count | Should -Be 2
+                $result.R53AccessKey | Should -BeExactly 'new-key'
+                $result.R53SecretKeyInsecure | Should -BeExactly 'new-secret'
+            }
+
+            It "Removes conflicting parameters for multiple plugins" {
+                @{
+                    R53ProfileName = 'old-profile'
+                    AZAccessToken = 'old-token'
+                } | Export-CliXml $pDataFile
+                $pargs = @{
+                    R53AccessKey = 'new-key'
+                    R53SecretKeyInsecure = 'new-secret'
+                    AZAppUsername = 'new-user'
+                    AZAppPasswordInsecure = 'new-pass'
+                }
+                Export-PluginArgs $pargs Route53,Azure
+                $result = Import-Clixml $pDataFile
+
+                $result.Keys.Count | Should -Be 4
+                $result.R53AccessKey | Should -BeExactly 'new-key'
+                $result.R53SecretKeyInsecure | Should -BeExactly 'new-secret'
+                $result.AZAppUsername | Should -BeExactly 'new-user'
+                $result.AZAppPasswordInsecure | Should -BeExactly 'new-pass'
+            }
+
+            It "Does not change parameters unassociated with plugin" {
+                @{
+                    R53ProfileName = 'old-profile'
+                    AZAccessToken = 'old-token'
+                } | Export-CliXml $pDataFile
+                $pargs = @{
+                    R53ProfileName = 'new-profile'
+                }
+                Export-PluginArgs $pargs Route53
+                $result = Import-Clixml $pDataFile
+
+                $result.Keys.Count | Should -Be 2
+                $result.R53ProfileName | Should -BeExactly 'new-profile'
+                $result.AZAccessToken | Should -BeExactly 'old-token'
+            }
+
+            It "Includes all new parameters unassociated with plugin" {
+                @{
+                    R53ProfileName = 'old-profile'
+                } | Export-CliXml $pDataFile
+                $pargs = @{
+                    R53ProfileName = 'new-profile'
+                    FakeParam1 = 'fake1'
+                    FakeParam2 = 'fake2'
+                }
+                Export-PluginArgs $pargs Route53
+                $result = Import-Clixml $pDataFile
+
+                $result.Keys.Count | Should -Be 3
+                $result.R53ProfileName | Should -BeExactly 'new-profile'
+                $result.FakeParam1 | Should -BeExactly 'fake1'
+                $result.FakeParam2 | Should -BeExactly 'fake2'
+            }
 
         }
 
