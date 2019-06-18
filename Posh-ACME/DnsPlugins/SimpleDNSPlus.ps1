27f2abd1bda4cea61c2534d56a746cbc873a7930
Ryan Bolger <z97y9drumh@snkmail.com>
2019-06-17 19:07:58 -0700

    fixed bug in check for existing TXT record, changed SdnsServer to SdnsApiRoot to allow for custom paths, added SdnsIgnoreCert option, added SdnsCred secure parameter option, renamed SdnsSecret to SdnsPassword, updated readme with changes and additional guidance on auth/TLS

--- Posh-ACME/DnsPlugins/SimpleDNSPlus.ps1
+++ Posh-ACME/DnsPlugins/SimpleDNSPlus.ps1
@@ -1,47 +1,65 @@
-
 function Add-DnsTxtSimpleDNSPlus {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Secure')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword','')]
     param(
-        [Parameter(Mandatory, Position = 0)]
+        [Parameter(Mandatory,Position=0)]
         [string]$RecordName,
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory,Position=1)]
         [string]$TxtValue,
-        [Parameter(Mandatory, Position = 2)]
-        [string]$SdnsServer,
-        [Parameter(Mandatory, Position = 3)]
+        [Parameter(Mandatory)]
+        [string]$SdnsApiRoot,
+        [Parameter(ParameterSetName='Secure')]
+        [pscredential]$SdnsCred,
+        [Parameter(ParameterSetName='Insecure',Mandatory)]
         [string]$SdnsUser,
-        [Parameter(Mandatory, Position = 4)]
-        [string]$SdnsSecret,
+        [Parameter(ParameterSetName='Insecure',Mandatory)]
+        [string]$SdnsPassword,
+        [switch]$SdnsIgnoreCert,
         [Parameter(ValueFromRemainingArguments)]
         $ExtraParams
     )
-	Ignore-SelfSignedCerts
-    $apiRoot = "https://$($SdnsServer)/v2/zones"
-	$p = $SdnsSecret | ConvertTo-SecureString -asPlainText -Force
-	$credential = New-Object System.Management.Automation.PSCredential($SdnsUser, $p)
 
+    $apiRoot = "$($SdnsApiRoot)/zones"
 
-    $zone = Find-SimpleDNSPlusZone -RecordName $RecordName -SdnsServer $SdnsServer -SdnsUser $SdnsUser -SdnsSecret $SdnsSecret
-    $recShort = ($RecordName -split ".$zone")[0]
+    # create a pscredential from insecure args if necessary
+    if ('Insecure' -eq $PSCmdlet.ParameterSetName) {
+        $secpass = ConvertTo-SecureString $SdnsPassword -AsPlainText -Force
+        $SdnsCred = New-Object PSCredential ($SdnsUser,$secpass)
+    }
+    $credSplat = @{}
+    if ($SdnsCred) { $credSplat.Credential = $SdnsCred }
 
-	# Get a list of existing TXT records for this record name
     try {
-		$recs = (Invoke-RestMethod "$apiRoot/$zone/records" -Credential $credential @script:UseBasic -EA Stop ) `
-			| Where-Object {$_.Type -eq "TXT" -And $_.Name -eq $RecordName}	
-	} catch { throw }
-	if (-not $recs -or $TxtValue -notin $recs.data) {
-        $bodyJson = "[{`"Name`":`"$RecordName`",`"Type`":`"TXT`",`"TTL`":600,`"Data`":`"$TxtValue`"}]"
+        # ignore cert validation for the duration of the call
+        if ($SdnsIgnoreCert) { Set-SdnsCertIgnoreOn }
 
+        $zone = Find-SimpleDNSPlusZone -RecordName $RecordName -SdnsApiRoot $SdnsApiRoot -CredSplat $credSplat
+
+        # Get a list of existing TXT records for this record name
         try {
-            Write-Debug "Sending $bodyJson"
-            Invoke-RestMethod "$apiRoot/$zone/records" `
-                -Method Patch -Credential $credential -Body $bodyJson `
-                -ContentType 'application/json' @script:UseBasic -EA Stop | Out-Null
+            $recs = (Invoke-RestMethod "$apiRoot/$zone/records" @credSplat @script:UseBasic -EA Stop ) |
+                Where-Object {$_.Type -eq "TXT" -And $_.Name -eq $RecordName}
         } catch { throw }
 
-    } else {
-        Write-Debug "Record $RecordName already contains $TxtValue. Nothing to do."
+        if (-not $recs -or "`"$TxtValue`"" -notin $recs.data) {
+            $bodyJson = ConvertTo-Json @(@{Name=$RecordName;Type='TXT';TTL=600;'Data'=$TxtValue}) -Compress
+
+            try {
+                Write-Debug "Sending $bodyJson"
+                Invoke-RestMethod "$apiRoot/$zone/records" `
+                    -Method Patch @credSplat -Body $bodyJson `
+                    -ContentType 'application/json' @script:UseBasic -EA Stop | Out-Null
+            } catch { throw }
+
+        } else {
+            Write-Debug "Record $RecordName already contains $TxtValue. Nothing to do."
+        }
+
+    } finally {
+        # return cert validation back to normal
+        if ($SdnsIgnoreCert) { Set-SdnsCertIgnoreOff }
     }
+
 
     <#
     .SYNOPSIS
@@ -56,76 +74,111 @@ function Add-DnsTxtSimpleDNSPlus {
     .PARAMETER TxtValue
         The value of the TXT record.
 
-    .PARAMETER SdnsServer
-        The Simple DNS Plus Server Name or IP.
+    .PARAMETER SdnsApiRoot
+        The root URL of the Simple DNS Plus Server API. For example, http://dns.example.com:8053 or http://dns.example.com:8053/v2
+
+    .PARAMETER SdnsCred
+        The HTTP API credentials required to authenticate.
 
     .PARAMETER SdnsUser
-        The Simple DNS Plus HTTP API Username.
+        The HTTP API Username.
 
     .PARAMETER SdnsSecret
-        The Simple DNS Plus HTTP API Password/Secret.
+        The HTTP API Password.
+
+    .PARAMETER SdnsIgnoreCert
+        Use this switch to prevent certificate errors when your Simple DNS Plus server is using a self-signed or other untrusted SSL certificate. When passing parameters via hashtable, set it as a boolean such as @{SdnsIgnoreCert=$true}.
 
     .PARAMETER ExtraParams
         This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
 
     .EXAMPLE
-        Add-DnsTxtSimpleDNSPlus '_acme-challenge.site1.example.com' '199.123.123.123' 'superuser' 'supersecretpassword'
+        Add-DnsTxtSimpleDNSPlus '_acme-challenge.example.com' 'txtvalue' -SdnsApiRoot http://dns.example.com:8053
 
-        Adds a TXT record for the specified site with the specified value.
+        Adds a TXT record using anonymous authentication.
+
+    .EXAMPLE
+        $pArgs = @{ SdnsApiRoot = 'http://dns.example.com:8053'; SdnsCred = (Get-Credential); SdnsIgnoreCert = $true }
+        PS C:\>Add-DnsTxtSimpleDNSPlus '_acme-challenge.site1.example.com' 'txtvalue' @pArgs
+
+        Adds a TXT record using credentials and ignores certificate validation.
+
+    .EXAMPLE
+        $pArgs = @{ SdnsApiRoot = 'http://dns.example.com:8053'; SdnsUser = 'admin'; SdnsPassword = 'xxxxxxxx' }
+        PS C:\>Add-DnsTxtSimpleDNSPlus '_acme-challenge.site1.example.com' 'txtvalue' @pArgs
+
+        Adds a TXT record using plain text credentials.
     #>
 }
 
 function Remove-DnsTxtSimpleDNSPlus {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Secure')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword','')]
     param(
-        [Parameter(Mandatory, Position = 0)]
+        [Parameter(Mandatory,Position=0)]
         [string]$RecordName,
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory,Position=1)]
         [string]$TxtValue,
-        [Parameter(Mandatory, Position = 2)]
-        [string]$SdnsServer,
-        [Parameter(Mandatory, Position = 3)]
+        [Parameter(Mandatory)]
+        [string]$SdnsApiRoot,
+        [Parameter(ParameterSetName='Secure')]
+        [pscredential]$SdnsCred,
+        [Parameter(ParameterSetName='Insecure',Mandatory)]
         [string]$SdnsUser,
-        [Parameter(Mandatory, Position = 4)]
-        [string]$SdnsSecret,
+        [Parameter(ParameterSetName='Insecure',Mandatory)]
+        [string]$SdnsPassword,
+        [switch]$SdnsIgnoreCert,
         [Parameter(ValueFromRemainingArguments)]
         $ExtraParams
     )
-	
-	Ignore-SelfSignedCerts
-    $apiRoot = "https://$($SdnsServer)/v2/zones"
-	$p = $SdnsSecret | ConvertTo-SecureString -asPlainText -Force
-	$credential = New-Object System.Management.Automation.PSCredential($SdnsUser, $p)
 
-	$zone = Find-SimpleDNSPlusZone -RecordName $RecordName -SdnsServer $SdnsServer -SdnsUser $SdnsUser -SdnsSecret $SdnsSecret
+    $apiRoot = "$($SdnsApiRoot)/zones"
 
-    $recShort = ($RecordName -split ".$zone")[0]
+    # create a pscredential from insecure args if necessary
+    if ('Insecure' -eq $PSCmdlet.ParameterSetName) {
+        $secpass = ConvertTo-SecureString $SdnsPassword -AsPlainText -Force
+        $SdnsCred = New-Object PSCredential ($SdnsUser,$secpass)
+    }
+    $credSplat = @{}
+    if ($SdnsCred) { $credSplat.Credential = $SdnsCred }
 
-    # Get a list of existing Challenge TXT records for this record name, 
-	# Simple DNS Plus API returns full fqdns as each record, so no need to parse/shorten it. 
     try {
-        $recs = (Invoke-RestMethod "$apiRoot/$zone/records" -Credential $credential @script:UseBasic -EA Stop ) `
-			| Select-Object -Property * -ExcludeProperty TTL `
-			| Where-Object {$_.Type -eq "TXT" -And $_.Name -eq $RecordName}	
-    } catch { throw }
-    if (-not $recs -or $TxtValue -notin $recs.Data) {
-        Write-Debug "Record $RecordName with value $TxtValue doesn't exist. Nothing to do."
-    } else {
-        # Txt record values in simple dns plus contains double quotes encasing the string. 
-		# So for it to JSON properly, we must remove those double quotes from the string.
-		$recs | foreach {$_.Data = $_.Data -replace '"',''}
+        # ignore cert validation for the duration of the call
+        if ($SdnsIgnoreCert) { Set-SdnsCertIgnoreOn }
 
-		$recToDelete = $recs | WHERE-Object {$_.Data -eq $TxtValue} 
-		$recToDelete | Add-Member -NotePropertyName Remove -NotePropertyValue $true
-		$bodyJson = ConvertTo-Json @($recToDelete) -Compress
+        $zone = Find-SimpleDNSPlusZone -RecordName $RecordName -SdnsApiRoot $SdnsApiRoot -CredSplat $credSplat
 
+        # Get a list of existing Challenge TXT records for this record name,
+        # Simple DNS Plus API returns full fqdns as each record, so no need to parse/shorten it.
         try {
-            Write-Debug "Sending $bodyJson"
-            Invoke-RestMethod "$apiRoot/$zone/records" `
-                -Method Patch -Credential $credential -Body $bodyJson `
-                -ContentType 'application/json' @script:UseBasic -EA Stop | Out-Null
+            $recs = (Invoke-RestMethod "$apiRoot/$zone/records" @credSplat @script:UseBasic -EA Stop) |
+                Select-Object -Property * -ExcludeProperty TTL |
+                Where-Object { $_.Type -eq "TXT" -And $_.Name -eq $RecordName }
         } catch { throw }
 
+        if (-not $recs -or "`"$TxtValue`"" -notin $recs.Data) {
+            Write-Debug "Record $RecordName with value $TxtValue doesn't exist. Nothing to do."
+        } else {
+            # Txt record values in simple dns plus contains double quotes encasing the string.
+            # So for it to JSON properly, we must remove those double quotes from the string.
+            $recs | ForEach-Object { $_.Data = $_.Data -replace '"','' }
+
+            $recToDelete = $recs | Where-Object {$_.Data -eq $TxtValue}
+            $recToDelete | Add-Member -NotePropertyName Remove -NotePropertyValue $true
+            $bodyJson = ConvertTo-Json @($recToDelete) -Compress
+
+            try {
+                Write-Debug "Sending $bodyJson"
+                Invoke-RestMethod "$apiRoot/$zone/records" `
+                    -Method Patch @credSplat -Body $bodyJson `
+                    -ContentType 'application/json' @script:UseBasic -EA Stop | Out-Null
+            } catch { throw }
+
+        }
+
+    } finally {
+        # return cert validation back to normal
+        if ($SdnsIgnoreCert) { Set-SdnsCertIgnoreOff }
     }
 
 
@@ -142,22 +195,40 @@ function Remove-DnsTxtSimpleDNSPlus {
     .PARAMETER TxtValue
         The value of the TXT record.
 
-    .PARAMETER SdnsServer
-        The Simple DNS Plus Server Name or IP.
+    .PARAMETER SdnsApiRoot
+        The root URL of the Simple DNS Plus Server API. For example, http://dns.example.com:8053 or http://dns.example.com:8053/v2
+
+    .PARAMETER SdnsCred
+        The HTTP API credentials required to authenticate.
 
     .PARAMETER SdnsUser
-        The Simple DNS Plus HTTP API Username.
+        The HTTP API Username.
 
     .PARAMETER SdnsSecret
-        The Simple DNS Plus HTTP API Password/Secret.
+        The HTTP API Password.
+
+    .PARAMETER SdnsIgnoreCert
+        Use this switch to prevent certificate errors when your Simple DNS Plus server is using a self-signed or other untrusted SSL certificate. When passing parameters via hashtable, set it as a boolean such as @{SdnsIgnoreCert=$true}.
 
     .PARAMETER ExtraParams
         This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
 
     .EXAMPLE
-        Remove-DnsTxtSimpleDNSPlus '_acme-challenge.site1.example.com' '199.123.123.123' 'superuser' 'supersecretpassword'
+        Remove-DnsTxtSimpleDNSPlus '_acme-challenge.example.com' 'txtvalue' -SdnsApiRoot http://dns.example.com:8053
 
-        Removes a TXT record for the specified site with the specified value.
+        Removes a TXT record using anonymous authentication.
+
+    .EXAMPLE
+        $pArgs = @{ SdnsApiRoot = 'http://dns.example.com:8053'; SdnsCred = (Get-Credential); SdnsIgnoreCert = $true }
+        PS C:\>Remove-DnsTxtSimpleDNSPlus '_acme-challenge.site1.example.com' 'txtvalue' @pArgs
+
+        Removes a TXT record using credentials and ignores certificate validation.
+
+    .EXAMPLE
+        $pArgs = @{ SdnsApiRoot = 'http://dns.example.com:8053'; SdnsUser = 'admin'; SdnsPassword = 'xxxxxxxx' }
+        PS C:\>Remove-DnsTxtSimpleDNSPlus '_acme-challenge.site1.example.com' 'txtvalue' @pArgs
+
+        Removes a TXT record using plain text credentials.
     #>
 }
 
@@ -182,56 +253,20 @@ function Save-DnsTxtSimpleDNSPlus {
 ############################
 # Helper Functions
 ############################
-function Ignore-SelfSignedCerts {
-	try {
-		Write-Host "Adding TrustAllCertsPolicy type." -ForegroundColor White
-		Add-Type -TypeDefinition @" 
-	using System.Net;
-	using System.Security.Cryptography.X509Certificates;
-	public class TrustAllCertsPolicy : ICertificatePolicy {
-	public bool CheckValidationResult(
-	ServicePoint srvPoint, X509Certificate certificate,
-	WebRequest request, int certificateProblem) {
-	return true;
-	}
-	}
-		"@
-		Write-Host "TrustAllCertsPolicy type added." -ForegroundColor White
-	}
-	catch {
-		Write-Host $_ -ForegroundColor "Yellow"
-	}
-		[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
-    <#
-    .SYNOPSIS
-        Because most Simple DNS Plus Server installations have Self-Signed Certs or Invalid Certs for the HTTPS API, 
-		it is necessary to allow Powershell Invoke-RestRestMethod to make Https Calls without failing https.
+# https://simpledns.com/help/how-to-use-the-http-api
 
-    .DESCRIPTION
-        Allow All Certs regardless of validation, and fix powershell issue where sometimes TLS10/11/12 not enabled causing 
-		requests to fail.
-
-    .EXAMPLE
-        Ignore-SelfSignedCerts
-        Allows All Certs.
-    #>
-}
 function Find-SimpleDNSPlusZone {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory, Position = 0)]
         [string]$RecordName,
         [Parameter(Mandatory, Position = 1)]
-        [string]$SdnsServer,
-        [Parameter(Mandatory, Position = 2)]
-        [string]$SdnsUser,
-        [Parameter(Mandatory, Position = 3)]
-        [string]$SdnsSecret,
+        [string]$SdnsApiRoot,
+        [Parameter(Position = 2)]
+        [hashtable]$CredSplat
     )
 
-	Ignore-SelfSignedCerts
     # setup a module variable to cache the record to zone mapping
     # so it's quicker to find later
     if (!$script:SdnsRecordZones) { $script:SdnsRecordZones = @{} }
@@ -241,13 +276,11 @@ function Find-SimpleDNSPlusZone {
         return $script:SdnsRecordZones.$RecordName
     }
 
-    $apiRoot = "https://$($SdnsServer)/v2/zones"
-	$p = $SdnsSecret | ConvertTo-SecureString -asPlainText -Force
-	$credential = New-Object System.Management.Automation.PSCredential($SdnsUser, $p)
+    $apiRoot = "$($SdnsApiRoot)/zones"
 
     # get the list of available zones
     try {
-        $zones = (Invoke-RestMethod $apiRoot -Credential $credential @script:UseBasic -EA Stop) `
+        $zones = (Invoke-RestMethod $apiRoot @credSplat @script:UseBasic -EA Stop) `
             | Where-Object {$_.Type -eq "primary"} `
             | Select-Object -ExpandProperty Name
     } catch { throw }
@@ -274,29 +307,43 @@ function Find-SimpleDNSPlusZone {
     }
 
     return $null
+}
 
-    <#
-    .SYNOPSIS
-        Finds the appropriate DNS zone for the supplied record
+# These cert ignore helpers rely on some TLS initialization code that runs during the
+# module import. So if you're dot sourcing the plugin file to test, you need to account
+# for that.
+function Set-SdnsCertIgnoreOn {
+    [CmdletBinding()]
+    param()
 
-    .DESCRIPTION
-        Finds the appropriate DNS zone for the supplied record
+    if ($script:SkipCertSupported) {
+        # Core edition
+        if (-not $script:UseBasic.SkipCertificateCheck) {
+            # temporarily set skip to true
+            $script:UseBasic.SkipCertificateCheck = $true
+            # remember that we did
+            $script:SdnsUnsetIgnoreAfter = $true
+        }
+    } else {
+        # Desktop edition
+        Write-Debug "Ignoring certs"
+        [CertValidation]::Ignore()
+    }
+}
 
-    .PARAMETER RecordName
-        The fully qualified name of the TXT record.
+function Set-SdnsCertIgnoreOff {
+    [CmdletBinding()]
+    param()
 
-    .PARAMETER SdnsServer
-        The Simple DNS Plus Server Name or IP.
-
-    .PARAMETER SdnsUser
-        The Simple DNS Plus HTTP API Username.
-
-    .PARAMETER SdnsSecret
-        The Simple DNS Plus HTTP API Password/Secret.
-
-    .EXAMPLE
-        Find-SimpleDnsPlusZone -RecordName '_acme-challenge.site1.example.com' -SdnsServer '199.123.123.123' -SdnsUser 'adminuser' -SdnsSecret 'SuperSecretPassword'
-
-        Finds the appropriate DNS zone for the supplied record
-    #>
+    if ($script:SkipCertSupported) {
+        # Core edition
+        if ($script:SdnsUnsetIgnoreAfter) {
+            $script:UseBasic.SkipCertificateCheck = $false
+            Remove-Variable SdnsUnsetIgnoreAfter -Scope Script
+        }
+    } else {
+        # Desktop edition
+        Write-Debug "Un-Ignoring certs"
+        [CertValidation]::Restore()
+    }
 }
