8324a3e33575bc0626b4ca07ba116ff5d1a45add
Oliver Karstoft <orkarstoft@gmail.com>
2019-10-13 10:14:02 +0200

    Add-func changed to use Find-UEZone insted of regex

--- Posh-ACME/DnsPlugins/Unoeuro.ps1
+++ Posh-ACME/DnsPlugins/Unoeuro.ps1
@@ -21,23 +21,39 @@ function Add-DnsTxtUnoEuro {
         'ttl' = 3600
         'priority' = 0
     } | ConvertTo-Json
-    $DomainRegex = '([0-9a-z-]{2,}\.[0-9a-z-]{2,3}\.[0-9a-z-]{2,3}|[0-9a-z-]{2,}\.[0-9a-z-]{2,3})$'
-    $UEDomain = [Regex]::Match($RecordName, $DomainRegex).value
     $UEDNSExists = $false
 
+    Write-Debug "Finding DNS Zone"
+    if (-not ($UEDomain = Find-UEZone $RecordName $UEAccount $UEAccount)) {
+        Write-Debug "Unable to find matching zone for $recordName."
+        throw "Unable to find matching zone for $RecordName."
+    }
+    Write-Debug "Found $UEDomain. Isolating."
+    $UESubDomain = $RecordName -ireplace [regex]::Escape(".$UEDomain"), [string]::Empty
+    Write-Debug "Accepted domain $UEDomain and record $UESubDomain"
+
     # check for an existing record
-    $UEResponse = Invoke-RestMethod $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/ -Method Get -ContentType 'application/json' @script:UseBasic
-    
+    Write-Debug "Running: GET $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/"
+    try {
+        $UEResponse = Invoke-RestMethod $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/ -Method Get -ContentType 'application/json'
+    }
+    catch {
+        Write-Debug $_
+        throw
+    }
+
+    Write-Debug "Response: $UEResponse"
     foreach ($UEDNSRecord in $UEResponse.records) {
-        if ($UEDNSRecord.name -eq ([Regex]::Replace($RecordName, [Regex]::Match($RecordName, '.'+$DomainRegex).value, ''))) {
+        Write-Debug "Record: $UEDNSRecord looking for $UESubDomain"
+        if ($UEDNSRecord.name -eq $UESubDomain) {
             $UEDNSExists = $true
         }
     }
 
     if (!$UEDNSExists) {
         Write-Debug "Record needs to be created."
-        Write-Debug "Running URL: $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/"
-        Invoke-RestMethod $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/ -Method Post -Body $UERequestObj -ContentType 'application/json' @script:UseBasic | Out-Null
+        Write-Debug "POST: $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/"
+        Invoke-RestMethod $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$UEdomain/dns/records/ -Method Post -Body $UERequestObj -ContentType 'application/json'
     } else {
         Write-Debug "Record $RecordName with value $TxtValue already exists. Nothing to do."
     }
@@ -134,5 +150,90 @@ function Save-DnsTxtUnoeuro {
         This provider does not require calling this function to commit changes to DNS records.
     .PARAMETER ExtraParams
         This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+    #>
+}
+
+############################
+# Helper Functions
+############################
+
+function Find-UEZone {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string]$RecordName,
+        [Parameter(Mandatory, Position = 1)]
+        [string]$UEAccount,
+        [Parameter(Mandatory, Position = 2)]
+        [string]$UEAPIKey
+    )
+
+    $UEApiRoot = 'https://api.unoeuro.com/1'
+
+    # setup a module variable to cache the record to zone mapping
+    # so it's quicker to find later
+    if (!$script:UERecordZones) { $script:UERecordZones = @{} }
+
+    # check for the record in the cache
+    if ($script:UERecordZones.ContainsKey($RecordName)) {
+        Write-Debug "Test already ran once. Using cache to speed up process."
+        return $script:UERecordZones.$RecordName
+    }
+
+    # We need to find the closest/deepest
+    # sub-zone that would hold the record rather than just adding it to the apex. So for something
+    # like _acme-challenge.site1.sub1.sub2.example.com, we'd look for zone matches in the following
+    # order:
+    # - site1.sub1.sub2.example.com
+    # - sub1.sub2.example.com
+    # - sub2.example.com
+    # - example.com
+    $pieces = $RecordName.Split('.')
+    for ($i = 1; $i -lt ($pieces.Count - 1); $i++) {
+        $zoneTest = "$( $pieces[$i..($pieces.Count-1)] -join '.' )"
+        Write-Debug "Checking $zoneTest"
+
+        try {
+            Write-Debug "Testing domain: $UEApiRoot/$UEAccount/$UEAPIKey/my/products/$zoneTest/dns/records/"
+            $domain = Invoke-RestMethod "$UEApiRoot/$UEAccount/$UEAPIKey/my/products/$zoneTest/dns/records/" -Method Get -ContentType 'application/json'
+        }
+        catch {
+            Write-Debug "Error was caught: $_"
+            # re-throw anything except a 404 because it means that something is very wrong.
+            # Unoeuro API returns code 400 no matter if it's wrong APIKey, Account or DNSZone. Therefore it's up to the next if-statement to sort it out.
+            if (404 -eq $_.Exception.Response.StatusCode.value__) {
+                Write-Debug "Error was 404. Throwing error."
+                throw
+            }
+            Write-Debug "Error was not 404 and will carry on."
+            continue
+        }
+
+        Write-Debug "Test: $domain"
+        if ($domain.status -eq '200') {
+            Write-Debug "Test complete. Accecpted: $zoneTest"
+            $script:UERecordZones.$RecordName = $zoneTest
+            return $zoneTest
+        } else {
+            Write-Debug "Found $zoneTest, but status was $($domain.status)"
+        }
+    }
+
+    return $null
+
+    <#
+    .SYNOPSIS
+        Finds the appropriate DNS zone for the supplied record
+    .DESCRIPTION
+        Finds the appropriate DNS zone for the supplied record
+    .PARAMETER RecordName
+        The fully qualified name of the TXT record.
+    .PARAMETER UEAccount
+        The Unoeuro account ID or name.
+    .PARAMETER UEAPIKey
+        The Unoeuro API Key.
+    .EXAMPLE
+        Find-UEZone -RecordName '_acme-challenge.site1.example.com' -GDKey 'asdfqwer12345678' -GDSecret 'dfasdasf3j42f'
+        Finds the appropriate DNS zone for the supplied record
     #>
 }
