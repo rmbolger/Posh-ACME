55ade39f8b62ba1a55d3272e570608a5848f742b
Ryan Bolger <z97y9drumh@snkmail.com>
2019-11-07 12:27:38 -0800

    Install-PACertificate now supports specifying the store location/name and whether the private key is exportable. (#193)

--- Posh-ACME/Public/Install-PACertificate.ps1
+++ Posh-ACME/Public/Install-PACertificate.ps1
@@ -2,7 +2,11 @@ function Install-PACertificate {
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipeline)]
-        [PSTypeName('PoshACME.PACertificate')]$PACertificate
+        [PSTypeName('PoshACME.PACertificate')]$PACertificate,
+        [ValidateSet('LocalMachine','CurrentUser')]
+        [string]$StoreLocation = 'LocalMachine',
+        [string]$StoreName = 'My',
+        [switch]$NotExportable
     )
 
     Process {
@@ -21,27 +25,51 @@ function Install-PACertificate {
             }
         }
 
-        Write-Verbose "Importing $($PACertificate.Subject) certificate to Windows certificate store."
-        Import-PfxCertInternal $PACertificate.PfxFullChain -PfxPass $PACertificate.PfxPass
+        Write-Verbose "Importing $($PACertificate.Subject) certificate to $StoreLocation\$StoreName."
+        $importArgs = @{
+            PfxFile = $PACertificate.PfxFullChain
+            PfxPass = $PACertificate.PfxPass
+            StoreLocation = $StoreLocation
+            StoreName = $StoreName
+            NotExportable = $NotExportable.IsPresent
+        }
+        Import-PfxCertInternal @importArgs
     }
 
 
     <#
     .SYNOPSIS
-        Install a Posh-ACME certificate into the local computer's certificate store.
+        Install a Posh-ACME certificate into a Windows certificate store.
 
     .DESCRIPTION
-        This can be used instead of the -Install parameter on New-PACertificate to import a certificate into the local computer's certificate store.
+        This can be used instead of the -Install parameter on New-PACertificate to import a certificate with more configurable options.
+
+    .PARAMETER PACertificate
+        The PACertificate object you want to import. This can be retrieved using Get-PACertificate and is also returned from things like New-PACertificate and Submit-Renewal.
+
+    .PARAMETER StoreLocation
+        Either 'LocalMachine' or 'CurrentUser'. Defaults to 'LocalMachine'.
+
+    .PARAMETER StoreName
+        The name of the certificate store to import to. Defaults to 'My'. The store must already exist and will not be created automatically.
+
+    .PARAMETER NotExportable
+        If specified, the private key will not be marked as Exportable.
 
     .EXAMPLE
         Install-PACertificate
 
-        Install the certificate associated with the currently selected order.
+        Install the certificate for the currently selected order to the default LocalMachine\My store.
 
     .EXAMPLE
         Get-PACertificate example.com | Install-PACertificate
 
-        Install the specified certificate.
+        Install the specified certificate to the default LocalMachine\My store.
+
+    .EXAMPLE
+        Install-PACertificate -StoreLocation 'CurrentUser' -NotExportable
+
+        Install the certificate for the currently selected order to the CurrentUser\My store and mark the private key as not exportable.
 
     .LINK
         Project: https://github.com/rmbolger/Posh-ACME
