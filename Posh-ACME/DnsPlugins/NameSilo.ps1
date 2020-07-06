fb4bccee57616911916fba46b0acfad6339fb50d
rkone <rob@morrisshome.com>
2020-07-06 12:09:25 -0400

    Create NameSilo.ps1

---
+++ Posh-ACME/DnsPlugins/NameSilo.ps1
@@ -0,0 +1,151 @@
+function Add-DnsTxtNameSilo {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$RecordName,
+        [Parameter(Mandatory,Position=1)]
+        [string]$TxtValue,
+        [Parameter(Mandatory,Position=3)]
+        [string]$Domain,
+        [Parameter(Mandatory,Position=4)]
+        [string]$NameSiloApiKey,
+        [Parameter(ValueFromRemainingArguments)]
+        $ExtraParams
+    )
+
+    $apiBase = 'https://www.namesilo.com/api'
+    $RecordName = $RecordName.ToLower()
+
+    try {       
+        $uri = "$apiBase/dnsAddRecord?version=1&type=xml&key=$($NameSiloApiKey)&domain=$($Domain)&rrtype=TXT&rrhost=$($RecordName)&rrvalue=$($TxtValue)"
+        $response = Invoke-RestMethod -Uri $uri
+    } catch { throw }
+
+    if ($response["namesilo"].reply.code -cne 300) {
+        throw "Failed to add TXT record: $($response["namesilo"].reply.detail)"
+    }
+    
+
+    <#
+    .SYNOPSIS
+        Add a DNS TXT record to NameSilo
+
+    .DESCRIPTION
+        Adds the TXT record to the NameSilo Zone
+
+    .PARAMETER RecordName
+        The fully qualified name of the TXT record.
+
+    .PARAMETER TxtValue
+        The value of the TXT record.
+
+    .PARAMETER Domain
+        The domain to add the record to.
+
+    .PARAMETER NameSiloApiKey
+        The API key for the NameSilo account. Created at https://www.namesilo.com/account/api-manager
+
+    .PARAMETER ExtraParams
+        This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+
+    .EXAMPLE
+        Add-DnsTxtNameSilo -RecordName "_acme-challenge.site1.example.com" -TxtValue "asdfqwer12345678" -Domain "example.com" -NameSiloApiKey "namesilo_api_key"
+
+        Adds a TXT record for the specified domain with the specified value.
+    #>
+}
+
+function Remove-DnsTxtNameSilo {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$RecordName,
+        [Parameter(Mandatory,Position=1)]
+        [string]$TxtValue,
+        [Parameter(Mandatory,Position=2)]
+        [string]$Domain,
+        [Parameter(Mandatory,Position=3)]
+        [string]$NameSiloApiKey,
+        [Parameter(ValueFromRemainingArguments)]
+        $ExtraParams
+    )
+
+    $apiBase = 'https://www.namesilo.com/api'
+    $RecordName = $RecordName.ToLower()
+    
+    try {
+        $response = Invoke-RestMethod "$apiBase/dnsListRecords?version=1&type=xml&key=$($NameSiloApiKey)&domain=$($Domain)"
+    } catch { throw }
+
+    $reply = $response["namesilo"].reply
+
+    if ($reply.code -cne 300) {
+        throw "Failed to list domain records: $($reply.detail)"
+    }
+    
+    $record = $reply.resource_record | Where-Object {$_.type -match "TXT" -and $_.host -match $($RecordName) -and $_.value -match $($TxtValue)}
+    if (@($record).Count -eq 1) {
+        # grab the record and delete it
+        try {
+            Write-Verbose "Deleting $RecordName with value $TxtValue"
+            $rrid = $record.record_id
+            $response2 = Invoke-RestMethod "$apiBase/dnsDeleteRecord?version=1&type=xml&key=$($NameSiloApiKey)&domain=$($Domain)&rrid=$($rrid)"
+            $reply2 = $response2["namesilo"].reply
+            if ($reply2.code -cne 300) {
+                throw "Failed to list domain records: $($reply2.detail)"
+            }    
+        } catch { throw }
+    } elseif (@($record).Count -gt 1) {
+        throw "multiple domain records found"
+    }
+    else {
+        # nothing to do
+        Write-Debug "Record $RecordName with value $TxtValue doesn't exist. Nothing to do."
+    }
+
+    <#
+    .SYNOPSIS
+        Remove a DNS TXT record from NameSilo
+
+    .DESCRIPTION
+        Removes the TXT record from the NameSilo zone
+
+    .PARAMETER RecordName
+        The fully qualified name of the TXT record.
+
+    .PARAMETER TxtValue
+        The value of the TXT record.
+
+    .PARAMETER Domain
+        The domain to add the record to.
+
+    .PARAMETER NameSiloApiKey
+        The API key for the NameSilo account. Created at https://www.namesilo.com/api-reference
+
+    .PARAMETER ExtraParams
+        This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+
+    .EXAMPLE
+        Remove-DnsTxtNameSilo '_acme-challenge.site1.example.com' 'asdfqwer12345678' 'NameSilo_api_client_id' 'NameSilo_api_client_secret'
+
+        Removes a TXT record for the specified domain with the specified value.
+    #>
+}
+
+function Save-DnsTxtNameSilo {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromRemainingArguments)]
+        $ExtraParams
+    )
+    <#
+    .SYNOPSIS
+        Not required
+
+    .DESCRIPTION
+        This provider does not require calling this function to save DNS records.
+
+    .PARAMETER ExtraParams
+        This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+    #>
+}
