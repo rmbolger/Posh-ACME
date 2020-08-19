a5f3228ebe5b1499ac573754a4a6a5a7be693a7b
Ryan Bolger <z97y9drumh@snkmail.com>
2020-08-19 10:11:15 -0700

    comment typo

--- Posh-ACME/Private/Import-Pem.ps1
+++ Posh-ACME/Private/Import-Pem.ps1
@@ -73,7 +73,7 @@ function Import-Pem {
                 $rsa.Prime1, $rsa.Prime2, $rsa.Exponent1, $rsa.Exponent2,
                 $rsa.Coefficient)
 
-        # check fo EC keys
+        # check for EC keys
         } elseif ($seq[0] -eq 1 -or
                   ($seq[0] -eq 0 -and $seq[1].Count -eq 2 -and
                    $seq[1][0] -eq [Org.BouncyCastle.Asn1.X9.X9ObjectIdentifiers]::IdECPublicKey) ) {
