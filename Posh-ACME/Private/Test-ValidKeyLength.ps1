039f402423b82e7bca012375acf1edf2cb8efddf
Ryan Bolger <z97y9drumh@snkmail.com>
2020-09-16 18:44:40 -0700

    added notes about new LE restrictions on RSA key sizes

--- Posh-ACME/Private/Test-ValidKeyLength.ps1
+++ Posh-ACME/Private/Test-ValidKeyLength.ps1
@@ -10,11 +10,14 @@ function Test-ValidKeyLength {
     # between 2048-4096 keys. So we'll limit to that until someone complains.
     # https://msdn.microsoft.com/en-us/library/system.security.cryptography.rsacryptoserviceprovider.keysize(v=vs.110).aspx
 
+    # 2020-09-17: LE is now restricting RSA key sizes to only 2048, 3072, and 4096
+    # https://community.letsencrypt.org/t/issuing-for-common-rsa-key-sizes-only/133839
+
     # For EC keys, LE's current Boulder server only supports P-256 and P-384, but may support P-521 by the time ACMEv2 is
     # production ready.
 
     # short circuit supported EC keys and common RSA keys
-    if ($KeyLength -in 'ec-256','ec-384','2048','4096') {
+    if ($KeyLength -in 'ec-256','ec-384','2048','3072','4096') {
         return $true
     }
 
