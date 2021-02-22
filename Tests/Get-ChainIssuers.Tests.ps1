3da3b61925bf3ce4230f980a8c6f6fd270ff0fba
Ryan Bolger <ryan-oss@xyto.cc>
2021-02-22 14:25:54 -0800

    fix Get-ChainIssuers test on non-Windows

--- Tests/Get-ChainIssuers.Tests.ps1
+++ Tests/Get-ChainIssuers.Tests.ps1
@@ -12,13 +12,13 @@ Describe "Get-ChainIssuers" {
             $issuers = Get-ChainIssuers -OrderFolder 'TestDrive:\acme.test\11111\example.com'
             $issuers | Should -HaveCount 3
             $issuers[0].issuer | Should -Be '(STAGING) Pretend Pear X1'
-            $issuers[0].filepath | Should -BeLike '*\chain0.cer'
+            $issuers[0].filepath | Should -BeLike '*chain0.cer'
             $issuers[0].index | Should -Be 1
             $issuers[1].issuer | Should -Be '(STAGING) Doctored Durian Root CA X3'
-            $issuers[1].filepath | Should -BeLike '*\chain0.cer'
+            $issuers[1].filepath | Should -BeLike '*chain0.cer'
             $issuers[1].index | Should -Be 0
             $issuers[2].issuer | Should -Be '(STAGING) Pretend Pear X1'
-            $issuers[2].filepath | Should -BeLike '*\chain1.cer'
+            $issuers[2].filepath | Should -BeLike '*chain1.cer'
             $issuers[2].index | Should -Be 0
         }
     }
