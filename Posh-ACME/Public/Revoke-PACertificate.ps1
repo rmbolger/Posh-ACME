ac74d4aee63c5ae9638f9cffff806c325526ebfb
Martin Thorpe <martin.thorpe@venafi.com>
2021-05-07 11:17:18 +0100

    Actually include the code to perform the revocation this time ...

--- Posh-ACME/Public/Revoke-PACertificate.ps1
+++ Posh-ACME/Public/Revoke-PACertificate.ps1
@@ -3,37 +3,85 @@ function Revoke-PACertificate {
     [OutputType('PoshACME.PACertificate')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText','')]
     param(
-        [Parameter(ParameterSetName='Specific',Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
-        [string]$MainDomain
+        [Parameter(ParameterSetName='MainDomain',Mandatory,Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string]$MainDomain,
+        [Parameter(ParameterSetName='CertPEM',Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string]$CertPEM,
+        [Parameter(ParameterSetName='MainDomain')]
+        [Parameter(ParameterSetName='CertPEM')]
+        [int]$Reason
     )
 
     Begin {
         # Make sure we have an account configured
         if (!(Get-PAAccount)) {
-            try { throw "No ACME account configured. Run Set-PAAccount or New-PAAccount first." }
-            catch { $PSCmdlet.ThrowTerminatingError($_) }
+            throw "No ACME account configured. Run Set-PAAccount or New-PAAccount first."
         }
     }
 
     Process {
 
-        $PACertificate = Get-PACertificate -MainDomain $MainDomain
+        if ($PSCmdlet.ParameterSetName -eq 'MainDomain') {
+            $PACertificate = Get-PACertificate -MainDomain $MainDomain
+            if (!$PACertificate) {
+                throw "No certificate with that name."
+            }
 
-        $certFile = $PACertificate.CertFile
-        $cert = Import-Pem -InputFile $certFile
-        $cert.GetEncoded()
+            $certFile = $PACertificate.CertFile
+            $cert = Import-Pem -InputFile $certFile
+        } else {
+            $cert = Import-Pem -InputString $CertPEM
 
+            # Pull the main domain out of the certificate
+            $MainDomain = $cert.SubjectDN
+            if (!$MainDomain -imatch "^CN=") {
+                throw "Could not extract main domain from certificate."
+            } else {
+                $MainDomain = $MainDomain -ireplace "^CN=", ""
+            }
+        }
+
+        $base64 = ConvertTo-Base64Url $cert.GetEncoded()
+
+        # build the protected header for the request
+        $header = @{
+            alg   = $acct.alg;
+            kid   = $acct.location;
+            nonce = $script:Dir.nonce;
+            url   = $script:Dir.revokeCert;
+        }
+
+        # build the payload
+        $payload = @{certificate=$base64}
+        if ($Reason -and $Reason -ge 1 -and $Reason -le 5) {
+            $payload.reason = $Reason
+        }
+
+        $payloadJson = $payload | ConvertTo-Json -Depth 5 -Compress
+
+        # send the request
+        try {
+            $response = Invoke-ACME $header $payloadJson $acct -EA Stop
+        } catch { throw }
+
+        Update-PAOrder $MainDomain
     }
 
     <#
     .SYNOPSIS
-        Revokes ACME certificate
+        Revoke an ACME certificate
 
     .DESCRIPTION
         Revokes a previously created ACME certificate.
 
     .PARAMETER MainDomain
-        The primary domain associated with the certificate. This is the domain that goes in the certificate's subject.
+        The primary domain associated with the certificate to be revoked. This is the domain that goes in the certificate's subject. Provide this argument if not providing CertPEM.
+
+    .PARAMETER CertPEM
+        The PEM-encoded certificate to be revoked. Provide this argument if not providing MainDomain.
+
+    .PARAMETER Reason
+        An optional reason for the revocation, one of 1 (User Key Compromised), 2 (CA Key Compromised), 3 (User Changed Affiliation), 4 (Certificate Superseded), 5 (Original Use No Longer Valid)
 
     .EXAMPLE
         Revoke-PACertificate site.example.com
