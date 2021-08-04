e38ac25afa2f22854880f81c70a3d228a83f3d65
Ryan Bolger <ryan-oss@xyto.cc>
2021-08-03 22:49:33 -0700

    tweaks to Export-PAAccountKey

--- Posh-ACME/Public/Export-PAAccountKey.ps1
+++ Posh-ACME/Public/Export-PAAccountKey.ps1
@@ -2,6 +2,7 @@ function Export-PAAccountKey {
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Position=0)]
+        [ValidateScript({Test-ValidFriendlyName $_ -ThrowOnFail})]
         [Alias('Name')]
         [string]$ID,
         [Parameter(Mandatory)]
@@ -9,12 +10,20 @@ function Export-PAAccountKey {
         [switch]$Force
     )
 
-    try {
-
+    Begin {
         # make sure we have a server configured
         if (-not (Get-PAServer)) {
-            throw "No ACME server configured. Run Set-PAServer first."
+            try { throw "No ACME server configured. Run Set-PAServer first." }
+            catch { $PSCmdlet.ThrowTerminatingError($_) }
         }
+
+        if ($Force) {
+            $ConfirmPreference = 'None'
+        }
+    }
+
+    Process {
+        trap { $PSCmdlet.ThrowTerminatingError($PSItem) }
 
         # throw an error if there's no current account and no ID passed in
         if (-not $ID -and -not ($acct = Get-PAAccount)) {
@@ -26,10 +35,13 @@ function Export-PAAccountKey {
             throw "Invalid account ID: $ID"
         }
 
+        # check if the output file exists
+        $fileExists = Test-Path $OutputFile -PathType Leaf
+
         # confirm overwrite unless -Force was specified
-        if (-not $Force -and (Test-Path $OutputFile -PathType Leaf) -and
-            -not $PSCmdlet.ShouldContinue("Overwrite?","$OutputFile already exists.")
-        ) {
+        if ($fileExists -and -not $Force -and
+            -not $PSCmdlet.ShouldContinue("Overwrite?","File already exists: $OutputFile"))
+        {
             Write-Verbose "Export account key aborted."
             return
         }
@@ -42,9 +54,6 @@ function Export-PAAccountKey {
         # export it
         Export-Pem $keypair $OutputFile
 
-    }
-    catch {
-        $PSCmdlet.ThrowTerminatingError($_)
     }
 
 
