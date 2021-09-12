45e029d045e4d10638e949f92a8d5dbded5397b6
Ryan Bolger <ryan-oss@xyto.cc>
2021-09-12 12:50:48 -0700

    fixed bug with Set-PAAccount -UseAltPluginEncryption that prevented properly re-encrypting plugin args for orders on the account

--- Posh-ACME/Private/Update-PluginEncryption.ps1
+++ Posh-ACME/Private/Update-PluginEncryption.ps1
@@ -18,11 +18,20 @@ function Update-PluginEncryption {
 
         # set the specified account as current and prepare to revert when we're done
         $revertToAccount = Get-PAAccount
-        Set-PAAccount -ID $ID
+        if ($revertToAccount -and $revertToAccount.id -ne $ID) {
+            Write-Debug "Temporarily switching to account '$ID'"
+            Set-PAAccount -ID $ID
+        }
 
-        # grab a copy of all the orders
-        $orders = Get-PAOrder -List
-        Write-Debug "Order data found for $($orders.Count) orders."
+        # grab a copy of the orders and plugin args before we break
+        # the ability to decrypt them
+        $orderData = @(Get-PAOrder -List | ForEach-Object {
+            @{
+                Order = $_
+                PluginArgs = ($_ | Get-PAPluginArgs)
+            }
+        })
+        Write-Debug "Order data found for $($orderData.Count) orders."
 
         # update and save the account with the new key
         if ($NewKey) {
@@ -37,15 +46,14 @@ function Update-PluginEncryption {
             Out-File $acctFile -Force -EA Stop
 
         # re-export all the plugin args
-        $orders | ForEach-Object {
-            $pArgs = $_ | Get-PAPluginArgs
-            Write-Debug "Re-exporting plugin args for order '$($_.Name)' with plugins $($_.Plugin -join ',') and data $($pArgs | ConvertTo-Json -Depth 5)"
-            Export-PluginArgs -Order $_ -PluginArgs $pArgs -IgnoreExisting
+        $orderData | ForEach-Object {
+            Write-Debug "Re-exporting plugin args for order '$($_.Order.Name)' with plugins $($_.Order.Plugin -join ',') and data $($pArgs | ConvertTo-Json -Depth 5)"
+            Export-PluginArgs @_ -IgnoreExisting
         }
 
         # revert the active account if necessary
         if ($revertToAccount -and $revertToAccount.id -ne $ID) {
-            Write-Debug "Reverting to previously active account"
+            Write-Debug "Reverting to previously active account '$($revertToAccount.id)'"
             Set-PAAccount -ID $revertToAccount.id
         }
     }
