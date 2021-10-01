1300a5e38f61c790992b3ea0909c18096a98713c
Steven Volckaert <steven.volckaert@skarabee.com>
2021-10-01 11:13:12 +0200

    Finish implementation of Add-DnsTxt function

--- Posh-ACME/Plugins/Combell.ps1
+++ Posh-ACME/Plugins/Combell.ps1
@@ -36,52 +36,35 @@ function Add-DnsTxt {
         $CombellApiKeyInsecure = (New-Object PSCredential ("userName", $CombellApiKey)).GetNetworkCredential().Password;
         $CombellApiSecretInsecure = (New-Object PSCredential ("userName", $CombellApiSecret)).GetNetworkCredential().Password;
     }
-    
 
-    # $RecordName contains the fully qualified domain name (FQDN); use it to get the domain name
-    # Todo get zone; see Remove-DnsTxt - Steven Volckaert, 1 October 2021.
-
-
-    # Docs @ https://api.combell.com/v2/documentation#tag/DNS-records/paths/~1dns~1{domainName}~1records/post
-
-    $txtRecords = Send-CombellHttpRequest `
-        -ApiKey $ApiKey `
-        -ApiSecret $ApiSecret `
-        -Method "GET" `
-        -Path "dns/$DomainName/records?type=TXT&record_name=$RecordName" `
-    | Where-Object { $_.record_name -eq $RecordName };
-    $txtRecords = $txtRecords | Where-Object { $_.record_name -eq $RecordName };
-    $numberOfTxtRecords = ($txtRecords | Measure-Object).Count;
+    $zoneName = Find-CombellZone $RecordName $CombellApiKeyInsecure $CombellApiSecretInsecure;
+    Write-Verbose "Found domain zone ""$zoneName"" for record ""$RecordName"".";
+    $shortRecordName = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.');
+    $txtRecords = Get-CombellTxtRecords $zoneName $shortRecordName $CombellApiKeyInsecure $CombellApiSecretInsecure;
+    $numberOfTxtRecords = $txtRecords.Length;
 
     if ($numberOfTxtRecords -eq 0) {
-        Write-Verbose "Domain ""$DomainName"" contains 0 TXT records that match record name ""$RecordName""; add TXT record { ""record_name"": ""$RecordName"", ""content"": ""$TxtValue"" }."
+        Write-Verbose "Domain ""$zoneName"" contains 0 TXT records that match record name ""$shortRecordName""; add TXT record { ""record_name"": ""$shortRecordName"", ""content"": ""$TxtValue"" }."
         $requestBody = @{
             type        = "TXT"
-            record_name = $RecordName
+            record_name = $shortRecordName
             ttl         = 60
-            content     = "$TxtValue"
+            content     = $TxtValue
         } | ConvertTo-Json -Compress
-        Write-Verbose "requestBody: $requestBody";
 
         Send-CombellHttpRequest `
-            -ApiKey $ApiKey `
-            -ApiSecret $ApiSecret `
+            -ApiKey $CombellApiKeyInsecure `
+            -ApiSecret $CombellApiSecretInsecure `
             -Body $requestBody `
-            -Method "POST" `
-            -Path "dns/$DomainName/records" | Out-Null;
-
-        $txtRecords = Send-CombellHttpRequest `
-            -ApiKey $ApiKey `
-            -ApiSecret $ApiSecret `
-            -Method "GET" `
-            -Path "dns/$DomainName/records?type=TXT&record_name=$RecordName" | Out-Null;
+            -Method POST `
+            -Path "dns/$zoneName/records" | Out-Null;
 
         return;
-    }   
+    }
 
-    $txtRecordToUpdate = $txtRecords | Select-Object -First 1;
+    $txtRecordToUpdate = $txtRecords | Select-Object -First 1;   
     $txtRecordDisplayName = "{ ""id"": ""$($txtRecordToUpdate.id)"", ""record_name"": ""$($txtRecordToUpdate.record_name)"", ""content"": ""$($txtRecordToUpdate.content)"" }";
-    Write-Verbose "Domain ""$DomainName"" contains $numberOfTxtRecords TXT record$(if ($numberOfTxtRecords -gt 1) { "s" }) that match$(if ($numberOfTxtRecords -eq 1) { "es" }) record name ""$RecordName""; update TXT record $txtRecordDisplayName with { ""content"": ""$TxtValue"" }."
+    Write-Verbose "Domain ""$zoneName"" contains $numberOfTxtRecords TXT record$(if ($numberOfTxtRecords -gt 1) { "s" }) that match$(if ($numberOfTxtRecords -eq 1) { "es" }) record name ""$shortRecordName""; update TXT record $txtRecordDisplayName with { ""content"": ""$TxtValue"" }."
 
     if ($txtRecordToUpdate.content -ceq $TxtValue) {
         Write-Verbose "TXT record $txtRecordDisplayName already has { ""content"": ""$TxtValue"" }; abort.";
@@ -90,17 +73,17 @@ function Add-DnsTxt {
 
     $requestBody = @{
         type        = "TXT"
-        record_name = $RecordName
+        record_name = $shortRecordName
         ttl         = 60
         content     = $TxtValue
     } | ConvertTo-Json -Compress;
 
     Send-CombellHttpRequest `
-        -ApiKey $ApiKey `
-        -ApiSecret $ApiSecret `
+        -ApiKey $CombellApiKeyInsecure `
+        -ApiSecret $CombellApiSecretInsecure `
         -Body $requestBody `
-        -Method "PUT" `
-        -Path "dns/$DomainName/records/$($txtRecordToUpdate.id)" | Out-Null;
+        -Method PUT `
+        -Path "dns/$zoneName/records/$($txtRecordToUpdate.id)" | Out-Null;
 
     <#
     .SYNOPSIS
@@ -139,101 +122,6 @@ function Add-DnsTxt {
     #>
 }
 
-# This function is provided only for testing purposes, it should probably be deleted when the plugin is ready.
-# See https://api.combell.com/v2/documentation#tag/DNS-records/paths/~1dns~1{domainName}~1records/get for
-# API documentation - Steven Volckaert, 29 September 2021.
-function Get-DnsRecords {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [string]$DomainName,
-        [Parameter(Mandatory)]
-        [string]$ApiKey,
-        [Parameter(Mandatory)]
-        [string]$ApiSecret
-    )
-
-    Write-Verbose "-DomainName: ""$DomainName""";
-    Send-CombellHttpRequest `
-        -ApiKey $ApiKey `
-        -ApiSecret $ApiSecret `
-        -Method "GET" `
-        -Path "dns/$DomainName/records"
-}
-
-function Add-DnsTxtTestRecord {
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0)]
-        [string]$DomainName = "skardev.com",
-        [Parameter(Position = 1)]
-        [string]$RecordName = "_acme-challenge",
-        [Parameter(Position = 2)]
-        [string]$TxtValue = "loremipsum",
-        [Parameter(Mandatory)]
-        [string]$ApiKey,
-        [Parameter(Mandatory)]
-        [string]$ApiSecret
-    )
-
-    $txtRecords = Send-CombellHttpRequest `
-        -ApiKey $ApiKey `
-        -ApiSecret $ApiSecret `
-        -Method "GET" `
-        -Path "dns/$DomainName/records?type=TXT&record_name=$RecordName";
-    $txtRecords = $txtRecords | Where-Object { $_.record_name -eq $RecordName };
-    $numberOfTxtRecords = ($txtRecords | Measure-Object).Count;
-
-    if ($numberOfTxtRecords -eq 0) {
-        Write-Verbose "Domain ""$DomainName"" contains 0 TXT records that match record name ""$RecordName""; add TXT record { ""record_name"": ""$RecordName"", ""content"": ""$TxtValue"" }."
-        $requestBody = @{
-            type        = "TXT"
-            record_name = $RecordName
-            ttl         = 60
-            content     = "$TxtValue"
-        } | ConvertTo-Json -Compress
-        Write-Verbose "requestBody: $requestBody";
-
-        Send-CombellHttpRequest `
-            -ApiKey $ApiKey `
-            -ApiSecret $ApiSecret `
-            -Body $requestBody `
-            -Method "POST" `
-            -Path "dns/$DomainName/records" | Out-Null;
-
-        $txtRecords = Send-CombellHttpRequest `
-            -ApiKey $ApiKey `
-            -ApiSecret $ApiSecret `
-            -Method "GET" `
-            -Path "dns/$DomainName/records?type=TXT&record_name=$RecordName" | Out-Null;
-
-        return;
-    }   
-
-    $txtRecordToUpdate = $txtRecords | Select-Object -First 1;
-    $txtRecordDisplayName = "{ ""id"": ""$($txtRecordToUpdate.id)"", ""record_name"": ""$($txtRecordToUpdate.record_name)"", ""content"": ""$($txtRecordToUpdate.content)"" }";
-    Write-Verbose "Domain ""$DomainName"" contains $numberOfTxtRecords TXT record$(if ($numberOfTxtRecords -gt 1) { "s" }) that match$(if ($numberOfTxtRecords -eq 1) { "es" }) record name ""$RecordName""; update TXT record $txtRecordDisplayName with { ""content"": ""$TxtValue"" }."
-
-    if ($txtRecordToUpdate.content -ceq $TxtValue) {
-        Write-Verbose "TXT record $txtRecordDisplayName already has { ""content"": ""$TxtValue"" }; abort.";
-        return;
-    }
-    
-    $requestBody = @{
-        type        = "TXT"
-        record_name = $RecordName
-        ttl         = 60
-        content     = $TxtValue
-    } | ConvertTo-Json -Compress;
-
-    Send-CombellHttpRequest `
-        -ApiKey $ApiKey `
-        -ApiSecret $ApiSecret `
-        -Body $requestBody `
-        -Method "PUT" `
-        -Path "dns/$DomainName/records/$($txtRecordToUpdate.id)" | Out-Null;
-}
-
 function Remove-DnsTxt {
     [CmdletBinding()]
     param(
@@ -259,18 +147,12 @@ function Remove-DnsTxt {
         $CombellApiSecretInsecure = (New-Object PSCredential ("userName", $CombellApiSecret)).GetNetworkCredential().Password;
     }
 
-    $zoneName = Find-CombellZone $RecordName $CombellApiKeyInsecure $CombellApiSecretInsecure
+    $zoneName = Find-CombellZone $RecordName $CombellApiKeyInsecure $CombellApiSecretInsecure;
     Write-Verbose "Found domain zone ""$zoneName"" for record ""$RecordName"".";
-    $shortRecordName = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
-    Write-Verbose "Short record name: ""$shortRecordName""";
-
-    $txtRecords = Send-CombellHttpRequest `
-        -ApiKey $CombellApiKeyInsecure `
-        -ApiSecret $CombellApiSecretInsecure `
-        -Method GET `
-        -Path "dns/$zoneName/records?type=TXT&record_name=$shortRecordName";
-    $txtRecords = $txtRecords | Where-Object { $_.record_name -eq $shortRecordName -and $_.content -ceq $TxtValue };
-    $numberOfTxtRecords = ($txtRecords | Measure-Object).Count;
+    $shortRecordName = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.');
+    $txtRecords = Get-CombellTxtRecords $zoneName $shortRecordName $CombellApiKeyInsecure $CombellApiSecretInsecure `
+    | Where-Object { $_.content -ceq $TxtValue };
+    $numberOfTxtRecords = $txtRecords.Length;
 
     if ($numberOfTxtRecords -eq 0) {
         Write-Verbose "Domain ""$zoneName"" contains 0 TXT records that match record name ""$shortRecordName"" and content ""$TxtValue""; abort."
@@ -349,6 +231,87 @@ function Save-DnsTxt {
 ############################
 # Helper Functions
 ############################
+
+function Find-CombellZone {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string]$RecordName,
+        [Parameter(Mandatory, Position = 1)]
+        [string]$CombellApiKeyInsecure,
+        [Parameter(Mandatory, Position = 2)]
+        [string]$CombellApiSecretInsecure
+    )
+
+    # Setup a module variable to cache the record to zone mapping, so it's quicker to find later.
+    if (!$script:CombellRecordZones) {
+        $script:CombellRecordZones = @{}
+    }
+
+    # If the cache contains $RecordName, return it.
+    if ($script:CombellRecordZones.ContainsKey($RecordName)) {
+        return $script:CombellRecordZones.$RecordName
+    }
+
+    # Not specifying the 'take' query parameter defaults the result set to a maximum 25 items (situation on 30
+    # September 2021). It is not clear from the documentation how you can get all domains in a single request, so I'm
+    # defaulting here to a 'take' parameter value of '1000'.
+    # If you find a better solution, feel free to submit an issue.
+    # See https://api.combell.com/v2/documentation#operation/Domains for more information.
+    # - Steven Volckaert, 30 September 2021.
+    try {
+        $zones = Send-CombellHttpRequest `
+            -ApiKey $CombellApiKeyInsecure `
+            -ApiSecret $CombellApiSecretInsecure `
+            -Method GET `
+            -Path "domains?take=1000";
+    }
+    catch { throw }
+
+    # We need to find the deepest sub-zone that can hold the record and add it there, except if there is only the apex
+    # zone. So for a $RecordName like _acme-challenge.site1.sub1.sub2.example.com, we need to search the zone from
+    # longest to shortest set of FQDNs contained in $zones, i.e. in the following order:
+    # - site1.sub1.sub2.example.com
+    # - sub1.sub2.example.com
+    # - sub2.example.com
+    # - example.com
+    # See https://poshac.me/docs/v4/Plugins/Plugin-Development-Guide/#zone-matching for more information.
+    # - Steven Volckaert, 30 September 2021.
+    $pieces = $RecordName.Split('.')
+    for ($i = 0; $i -lt ($pieces.Count - 1); $i++) {
+        $zoneTest = $pieces[$i..($pieces.Count - 1)] -join '.'
+        Write-Verbose "Checking $zoneTest"
+        if ($zoneTest -in $zones.domain_name) {
+            $zone = $zones | Where-Object { $_.domain_name -eq $zoneTest }
+            $script:CombellRecordZones.$RecordName = $zone.domain_name
+            return $zone.domain_name
+        }
+    }
+
+    throw "No domain zone found for ""$RecordName"".";
+}
+
+function Get-CombellTxtRecords {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string]$DomainName,
+        [Parameter(Mandatory, Position = 1)]
+        [string]$ShortRecordName,
+        [Parameter(Mandatory, Position = 2)]
+        [string]$CombellApiKeyInsecure,
+        [Parameter(Mandatory, Position = 3)]
+        [string]$CombellApiSecretInsecure
+    )
+
+    $txtRecords = Send-CombellHttpRequest `
+        -ApiKey $CombellApiKeyInsecure `
+        -ApiSecret $CombellApiSecretInsecure `
+        -Method GET `
+        -Path "dns/$DomainName/records?type=TXT&record_name=$ShortRecordName";
+
+    return @($txtRecords | Where-Object { $_.record_name -eq $ShortRecordName });
+}
 
 function Get-HMACSHA256Hash {
     [CmdletBinding()]
@@ -432,93 +395,4 @@ function Send-CombellHttpRequest {
         Write-Error "$Method $uri - $($_.Exception.Response.StatusCode.value__) ($($Stopwatch.ElapsedMilliseconds) ms) - $($_)";
         throw;
     }
-}
-
-function Find-CombellZone {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [string]$RecordName,
-        [Parameter(Mandatory, Position = 1)]
-        [string]$CombellApiKeyInsecure,
-        [Parameter(Mandatory, Position = 2)]
-        [string]$CombellApiSecretInsecure
-    )
-
-    # Setup a module variable to cache the record to zone mapping, so it's quicker to find later.
-    if (!$script:CombellRecordZones) {
-        $script:CombellRecordZones = @{}
-    }
-
-    # If the cache contains $RecordName, return it.
-    if ($script:CombellRecordZones.ContainsKey($RecordName)) {
-        return $script:CombellRecordZones.$RecordName
-    }
-
-    # Not specifying the 'take' query parameter defaults the result set to a maximum 25 items (situation on 30
-    # September 2021). It is not clear from the documentation how you can get all domains in a single request, so I'm
-    # defaulting here to a 'take' parameter value of '1000'.
-    # If you find a better solution, feel free to submit an issue.
-    # See https://api.combell.com/v2/documentation#operation/Domains for more information.
-    # - Steven Volckaert, 30 September 2021.
-    try {
-        $zones = Send-CombellHttpRequest `
-            -ApiKey $CombellApiKeyInsecure `
-            -ApiSecret $CombellApiSecretInsecure `
-            -Method GET `
-            -Path "domains?take=1000";
-    }
-    catch { throw }
-
-    # We need to find the deepest sub-zone that can hold the record and add it there, except if there is only the apex
-    # zone. So for a $RecordName like _acme-challenge.site1.sub1.sub2.example.com, we need to search the zone from
-    # longest to shortest set of FQDNs contained in $zones, i.e. in the following order:
-    # - site1.sub1.sub2.example.com
-    # - sub1.sub2.example.com
-    # - sub2.example.com
-    # - example.com
-    # See https://poshac.me/docs/v4/Plugins/Plugin-Development-Guide/#zone-matching for more information.
-    # - Steven Volckaert, 30 September 2021.
-    $pieces = $RecordName.Split('.')
-    for ($i = 0; $i -lt ($pieces.Count - 1); $i++) {
-        $zoneTest = $pieces[$i..($pieces.Count - 1)] -join '.'
-        Write-Verbose "Checking $zoneTest"
-        if ($zoneTest -in $zones.domain_name) {
-            $zone = $zones | Where-Object { $_.domain_name -eq $zoneTest }
-            $script:CombellRecordZones.$RecordName = $zone.domain_name
-            return $zone.domain_name
-        }
-    }
-
-    throw "No domain zone found for ""$RecordName"".";
-}
-
-function Get-CombellDnsRecords {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [string]$DomainName,
-        [string]$RecordType = $null,
-        [string]$ApiKey,
-        [string]$ApiSecret
-    )
-
-    # Docs @ https://api.combell.com/v2/documentation#tag/DNS-records/paths/~1dns~1{domainName}~1records/get
-    # HTTP GET https://api.combell.com/v2/dns/{domainName}/records?type=$RecordType
-
-    $requestPath = "dns/$DomainName/records";
-    if ([string]::IsNullOrEmpty($RecordType) -eq $false) {
-        $requestPath += "?type=$RecordType";
-    }
-    Write-Verbose "HTTP GET $requestPath";
-
-    $response = Send-CombellHttpRequest $requestPath -Method GET -ApiKey $ApiKey -ApiSecret $ApiSecret;
-
-    # TODO Print $response to output
-    $response | Format-List;
-
-    <#
-    .SYNOPSIS
-        Gets the DNS records of the specified domain name.
-    #>
 }
