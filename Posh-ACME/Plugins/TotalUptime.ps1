85536a6c86eb03b3bee3527ad2b6da5d3b1ab114
Christopher Simpson <christopher@1g5.co.uk>
2021-11-24 09:33:20 +0000

    Updated code formatting for TotalUptime Plugin

--- Posh-ACME/Plugins/TotalUptime.ps1
+++ Posh-ACME/Plugins/TotalUptime.ps1
@@ -3,9 +3,9 @@ function Get-CurrentPluginType { 'dns-01' }
 function Add-DnsTxt {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,Position=0)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$RecordName,
-        [Parameter(Mandatory,Position=1)]
+        [Parameter(Mandatory, Position = 1)]
         [string]$TxtValue,
         [Parameter(Mandatory)]
         [pscredential]$TotalUptimeCredential,
@@ -14,12 +14,10 @@ function Add-DnsTxt {
         $ExtraParams
     )
 
-    # Do work here to add the TXT record. Remember to add @script:UseBasic
-    # to all calls to Invoke-RestMethod or Invoke-WebRequest.
 
     #Find apex domain for record
     $DomainMetadata = Find-TotalUptimeDomain -RecordName $RecordName -TotalUptimeCredential $TotalUptimeCredential -TotalUptimeApiRoot $TotalUptimeApiRoot
-    if (-not $DomainMetadata){
+    if (-not $DomainMetadata) {
         throw "Unable to find TotalUptime DNS Zone for $RecordName"
     }
     Write-Debug "Found Domain ID $($DomainMetadata.id) for record $RecordName"
@@ -30,27 +28,29 @@ function Add-DnsTxt {
     if ($recShort -eq [string]::Empty) {
         $recShort = '@'
     }
+    Write-Debug "Stripped record name to $recShort"
 
     $DomainID = $DomainMetadata.id
 
     #Build Request
     $RequestUri = "$TotalUptimeApiRoot/CloudDNS/Domain/$DomainID/TXTRecord"
     $RequestHeaders = @{
-        Authorization=("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
-        Accept="application/json"
-        'Content-Type'="application/x-www-form-urlencoded"
+        Authorization  = ("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
+        Accept         = "application/json"
+        'Content-Type' = "application/x-www-form-urlencoded"
     }
     $RequestBody = @{
-        txtHostName=$recShort;
-        txtText=$TxtValue;
-        txtTTL=60;
+        txtHostName = $recShort;
+        txtText     = $TxtValue;
+        txtTTL      = 60;
     }
-    Write-Debug $RequestUri
     $Response = Invoke-RestMethod -Method Post -Uri $RequestUri -Headers $RequestHeaders -Body ($RequestBody | ConvertTo-Json -Compress) @script:UseBasic
 
     #failure throw error
-    if ($Response.status -ne 'Success'){
+    if ($Response.status -ne 'Success') {
         throw $Response.message
+    } else {
+        Write-Debug $Response.message
     }
 
     <#
@@ -58,7 +58,7 @@ function Add-DnsTxt {
         Add a DNS TXT record to TotalUptime
 
     .DESCRIPTION
-        Description for <My DNS Server/Provider>
+        Add a DNS TXT record to TotalUptime
 
     .PARAMETER RecordName
         The fully qualified name of the TXT record.
@@ -85,9 +85,9 @@ function Add-DnsTxt {
 function Remove-DnsTxt {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,Position=0)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$RecordName,
-        [Parameter(Mandatory,Position=1)]
+        [Parameter(Mandatory, Position = 1)]
         [string]$TxtValue,
         [Parameter(Mandatory)]
         [pscredential]$TotalUptimeCredential,
@@ -99,7 +99,7 @@ function Remove-DnsTxt {
 
     #Find apex domain for record
     $DomainMetadata = Find-TotalUptimeDomain -RecordName $RecordName -TotalUptimeCredential $TotalUptimeCredential -TotalUptimeApiRoot $TotalUptimeApiRoot
-    if (-not $DomainMetadata){
+    if (-not $DomainMetadata) {
         throw "Unable to find TotalUptime DNS Zone for $RecordName"
     }
 
@@ -110,22 +110,26 @@ function Remove-DnsTxt {
     }
 
     $txtRecord = Find-TotalUptimeTXTRecord -DomainID $DomainMetadata.id -RecordName $recShort -TxtContent $TxtValue -TotalUptimeCredential $TotalUptimeCredential -TotalUptimeApiRoot $TotalUptimeApiRoot
-    if (-not $txtRecord){
+    if (-not $txtRecord) {
         Write-Debug 'No record found, nothing to do'
-    }else{
+    }
+    else {
         $RecordID = $txtRecord.id
         $DomainID = $DomainMetadata.id
         #Build Request
         $RequestUri = "$TotalUptimeApiRoot/CloudDNS/Domain/$DomainID/TXTRecord/$RecordID"
         $RequestHeaders = @{
-            Authorization=("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
-            Accept="application/json"
+            Authorization = ("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
+            Accept        = "application/json"
         }
     
         $Response = Invoke-RestMethod -Method Delete -Uri $RequestUri -Headers $RequestHeaders
         
-        if ($Response.status -ne 'Success'){
+        # throw error on failure
+        if ($Response.status -ne 'Success') {
             throw $Response.message
+        } else {
+            Write-Debug $Response.message
         }
     }
 
@@ -186,63 +190,72 @@ function Save-DnsTxt {
 # API Documentation
 # https://totaluptime.com/api/v2/
 
+# Auth function
 function ConvertTo-TotalUptimeBasicHTTPAuthString {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
         [PSCredential]$Credential
-        )
+    )
     #Converts a PSCredential Object to a HTTP Basic Auth String
     $base64AuthString = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Credential.GetNetworkCredential().UserName, $Credential.GetNetworkCredential().Password)))
     return $base64AuthString
 }
 
+# Search Functions
+
 function Find-TotalUptimeDomain {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory,Position=0)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$RecordName,
         [Parameter(Mandatory)]
         [pscredential]$TotalUptimeCredential,
         [string]$TotalUptimeApiRoot = 'https://api.totaluptime.com'
     )
 
+    #retrieve domains in account
     $Domains = Get-TotalUptimeDomains -TotalUptimeCredential $TotalUptimeCredential -TotalUptimeApiRoot $TotalUptimeApiRoot
-
     $DomainNames = $Domains.rows.domainName
 
+    #check if domain is in domain list
     $pieces = $RecordName.Split('.')
-    for ($i=0; $i -lt ($pieces.Count-1); $i++) {
-        $domainTest = $pieces[$i..($pieces.Count-1)] -join '.'
+    for ($i = 0; $i -lt ($pieces.Count - 1); $i++) {
+        $domainTest = $pieces[$i..($pieces.Count - 1)] -join '.'
         Write-Debug "Checking $domainTest"
-        if ($domainTest -in $DomainNames){
+        if ($domainTest -in $DomainNames) {
+            Write-Debug "Found $domainTest"
             return $domains.rows | Where-Object domainName -eq $domainTest
         }
     }
+    Write-Debug "Unable to find matching domain"
     return $null
 }
 
 function Find-TotalUptimeTXTRecord {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory,Position=0)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$RecordName,
-        [Parameter(Mandatory,Position=1)]
+        [Parameter(Mandatory, Position = 1)]
         [string]$TxtContent,
-        [Parameter(Mandatory,Position=2)]
+        [Parameter(Mandatory, Position = 2)]
         [string]$DomainID,
         [Parameter(Mandatory)]
         [pscredential]$TotalUptimeCredential,
         [string]$TotalUptimeApiRoot = 'https://api.totaluptime.com'
     )
+    #retrieve TXT records for Domain
     $TXTRecords = Get-TotalUptimeTXTRecords -DomainID $DomainID -TotalUptimeCredential $TotalUptimeCredential -TotalUptimeApiRoot $TotalUptimeApiRoot
 
-    return $TXTRecords.rows | Where-Object {($_.txtHostName -eq $RecordName) -and ($_.txtText -eq $TxtContent)}
+    #filter by name an content
+    return $TXTRecords.rows | Where-Object { ($_.txtHostName -eq $RecordName) -and ($_.txtText -eq $TxtContent) }
     
 }
 
+# API Calls
 
-function Get-TotalUptimeDomains{
+function Get-TotalUptimeDomains {
     param(
         [Parameter(Mandatory)]
         [pscredential]$TotalUptimeCredential,
@@ -252,8 +265,8 @@ function Get-TotalUptimeDomains{
     #Build Request
     $RequestUri = "$TotalUptimeApiRoot/CloudDNS/Domain/All"
     $RequestHeaders = @{
-        Authorization=("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
-        Accept="application/json"
+        Authorization = ("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
+        Accept        = "application/json"
     }
     
     #Execute Request
@@ -263,7 +276,7 @@ function Get-TotalUptimeDomains{
 
 }
 
-function Get-TotalUptimeTXTRecords{
+function Get-TotalUptimeTXTRecords {
     param(
         [parameter(Mandatory = $true)]$DomainID,
         [pscredential]$TotalUptimeCredential,
@@ -273,10 +286,11 @@ function Get-TotalUptimeTXTRecords{
     #Build Request
     $RequestUri = "$TotalUptimeApiRoot/CloudDNS/Domain/$DomainID/TXTRecord/All"
     $RequestHeaders = @{
-        Authorization=("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
-        Accept="application/json"
+        Authorization = ("Basic {0}" -f (ConvertTo-TotalUptimeBasicHTTPAuthString -Credential $TotalUptimeCredential));
+        Accept        = "application/json"
     }
 
+    #Execute Request
     $Response = Invoke-RestMethod -Method Get -Uri $RequestUri -Headers $RequestHeaders @script:UseBasic
 
     return $Response
