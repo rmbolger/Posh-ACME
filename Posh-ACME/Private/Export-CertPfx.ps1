2c83810007e6dd278d1fecdb4a24bd76133ed7b2
Sebastian Johansson <sebastian.johansson@tietoevry.com>
2022-08-05 12:30:18 +0200

    Use modern PBES2 for private keys in PFX

--- Posh-ACME/Private/Export-CertPfx.ps1
+++ Posh-ACME/Private/Export-CertPfx.ps1
@@ -28,8 +28,13 @@ function Export-CertPfx {
         $FriendlyName = $cert.Subject.GetValueList([Org.BouncyCastle.Asn1.X509.X509Name]::CN)[0]
     }
 
-    # create a new Pkcs12Store
-    $store = New-Object Org.BouncyCastle.Pkcs.Pkcs12Store
+    # create a new Pkcs12StoreBuilder
+    $storebuilder = New-Object Org.BouncyCastle.Pkcs.Pkcs12StoreBuilder
+    # Certs could be left unencrypted since it's just the public part, but BouncyCastle does not support it - it also does not expose PKCS5 Scheme 2 for certs.
+    $storebuilder.SetCertAlgorithm([Org.BouncyCastle.Asn1.Pkcs.PkcsObjectIdentifiers]::PbeWithShaAnd3KeyTripleDesCbc.Id) | Out-Null
+    # Configure the Pkcs12Store to use PKCS5 Scheme 2 for private keys, using AES256CBC and HMAC-SHA256. 
+    $storebuilder.SetKeyAlgorithm([Org.BouncyCastle.Asn1.Nist.NistObjectIdentifiers]::IdAes256Cbc.Id, [Org.BouncyCastle.Asn1.Pkcs.PkcsObjectIdentifiers]::IdHmacWithSha256.Id) | Out-Null
+    $store = $storebuilder.Build()
 
     # add the private key
     try {
