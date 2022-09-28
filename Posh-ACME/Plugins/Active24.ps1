aa5c173563a9fc0b646db1d1dd4de12ed12da91d
Pavel Zakavec <pavel.zakavec@seznam.cz>
2022-09-28 15:54:15 +0200

    Added Plugin for Active24 DNS provider (#456)

---
+++ Posh-ACME/Plugins/Active24.ps1
@@ -0,0 +1,219 @@
+function Get-CurrentPluginType { 'dns-01' }
+
+function Add-DnsTxt {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$RecordName,
+        [Parameter(Mandatory,Position=1)]
+        [string]$TxtValue,
+        [Parameter(Mandatory,Position=2)]
+        [securestring]$A24Token,
+        [Parameter(ValueFromRemainingArguments)]
+        $ExtraParams
+    )
+
+    # get the plaintext version of the token
+    $A24TokenInsecure = [pscredential]::new('a',$A24Token).GetNetworkCredential().Password
+
+    $apiRoot = 'https://api.active24.com'
+    $restParams = @{
+        Headers = @{Authorization="Bearer $A24TokenInsecure"}
+        ContentType = 'application/json'
+    }
+
+    # get the zone name for our record
+    $zoneName = Find-A24Zone $RecordName $restParams
+    if ([String]::IsNullOrWhiteSpace($zoneName)) {
+        throw "Unable to find zone for $RecordName in account $acctID"
+    }
+    Write-Debug "Found zone $zoneName"
+
+    # get all the instances of the record
+    try {
+        $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+        $recs = (Invoke-RestMethod "$apiRoot/dns/$zoneName/records/v1" @restParams -Method Get) | ? {$_.name -eq $recShort -and $_.type -eq "TXT"}
+    } catch { throw }
+
+    if ($recs.Count -eq 0 -or $TxtValue -notin $recs.text) {
+        # add new record
+        try {
+            Write-Verbose "Adding a TXT record for $RecordName with value $TxtValue"
+            $bodyJson = @{name=$recShort;text=$TxtValue;ttl=300} | ConvertTo-Json -Compress
+            Invoke-RestMethod "$apiRoot/dns/$zoneName/txt/v1" -Method Post -Body $bodyJson @restParams | Out-Null
+        } catch { throw }
+    } else {
+        Write-Debug "Record $RecordName already contains $TxtValue. Nothing to do."
+    }
+
+    <#
+    .SYNOPSIS
+        Add a DNS TXT record to Active24.
+
+    .DESCRIPTION
+        Add a DNS TXT record to Active24
+
+    .PARAMETER RecordName
+        The fully qualified name of the TXT record.
+
+    .PARAMETER TxtValue
+        The value of the TXT record.
+
+    .PARAMETER A24Token
+        The access API token for Active24
+
+    .PARAMETER ExtraParams
+        This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+
+    .EXAMPLE
+        $token = Read-Host "Active24 Token" -AsSecureString
+        PS C:\>Add-DnsTxt '_acme-challenge.example.com' 'txt-value' $token
+
+        Adds a TXT record for the specified site with the specified value on Windows.
+    #>
+}
+
+function Remove-DnsTxt {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$RecordName,
+        [Parameter(Mandatory,Position=1)]
+        [string]$TxtValue,
+        [Parameter(Mandatory,Position=2)]
+        [securestring]$A24Token,
+        [Parameter(ValueFromRemainingArguments)]
+        $ExtraParams
+    )
+
+    # get the plaintext version of the token
+    $A24TokenInsecure = [pscredential]::new('a',$A24Token).GetNetworkCredential().Password
+
+    $apiRoot = 'https://api.active24.com'
+    $restParams = @{
+        Headers = @{Authorization="Bearer $A24TokenInsecure"}
+        ContentType = 'application/json'
+    }
+
+    # get the zone name for our record
+    $zoneName = Find-A24Zone $RecordName $restParams
+    if ([String]::IsNullOrWhiteSpace($zoneName)) {
+        throw "Unable to find zone for $RecordName in account $acctID"
+    }
+    Write-Debug "Found zone $zoneName"
+
+    # get all the instances of the record
+    try {
+        $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+        $recs = (Invoke-RestMethod "$apiRoot/dns/$zoneName/records/v1" @restParams -Method Get) | ? {$_.name -eq $recShort -and $_.type -eq "TXT"}
+    } catch { throw }
+
+    if ($recs.Count -eq 0 -or $TxtValue -notin $recs.text) {
+        Write-Debug "Record $RecordName with value $TxtValue doesn't exist. Nothing to do."
+    } else {
+        # delete record
+        try {
+            Write-Verbose "Removing TXT record for $RecordName with value $TxtValue"
+            $recID = ($recs | Where-Object { $_.text -eq $TxtValue }).hashId
+            Invoke-RestMethod "$apiRoot/dns/$zoneName/$recID/v1" -Method Delete @restParams | Out-Null
+        } catch { throw }
+    }
+
+    <#
+    .SYNOPSIS
+        Remove a DNS TXT record from Active24.
+
+    .DESCRIPTION
+        Remove a DNS TXT record from Active24.
+
+    .PARAMETER RecordName
+        The fully qualified name of the TXT record.
+
+    .PARAMETER TxtValue
+        The value of the TXT record.
+
+    .PARAMETER A24Token
+        The access API token for Active24.
+
+    .PARAMETER ExtraParams
+        This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+
+    .EXAMPLE
+        $token = Read-Host "Active24 Token" -AsSecureString
+        PS C:\>Remove-DnsTxt '_acme-challenge.example.com' 'txt-value' $token
+
+        Removes a TXT record for the specified site with the specified value on Windows.
+    #>
+}
+
+function Save-DnsTxt {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromRemainingArguments)]
+        $ExtraParams
+    )
+    <#
+    .SYNOPSIS
+        Not required.
+
+    .DESCRIPTION
+        This provider does not require calling this function to commit changes to DNS records.
+
+    .PARAMETER ExtraParams
+        This parameter can be ignored and is only used to prevent errors when splatting with more parameters than this function supports.
+    #>
+}
+
+############################
+# Helper Functions
+############################
+
+# API Docs
+# https://api.active24.com/swagger-ui.html
+
+function Find-A24Zone {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$RecordName,
+        [Parameter(Mandatory,Position=1)]
+        [hashtable]$RestParams
+    )
+
+    # setup a module variable to cache the record to zone mapping
+    # so it's quicker to find later
+    if (!$script:A24RecordZones) { $script:A24RecordZones = @{} }
+
+    # check for the record in the cache
+    if ($script:A24RecordZones.ContainsKey($RecordName)) {
+        return $script:A24RecordZones.$RecordName
+    }
+
+    $apiRoot = 'https://api.active24.com'
+
+    # Since the provider could be hosting both apex and sub-zones, we need to find the closest/deepest
+    # sub-zone that would hold the record rather than just adding it to the apex. So for something
+    # like _acme-challenge.site1.sub1.sub2.example.com, we'd look for zone matches in the following
+    # order:
+    # - site1.sub1.sub2.example.com
+    # - sub1.sub2.example.com
+    # - sub2.example.com
+    # - example.com
+
+    $pieces = $RecordName.Split('.')
+    for ($i=0; $i -lt ($pieces.Count-1); $i++) {
+        $zoneTest = $pieces[$i..($pieces.Count-1)] -join '.'
+        Write-Debug "Checking $zoneTest"
+        try {
+            # if the call succeeds, the zone exists, so we don't care about the actualy response
+            $null = Invoke-RestMethod "$apiRoot/dns/$zoneTest/records/v1" @RestParams -Method Get
+            $script:A24RecordZones.$RecordName = $zoneTest
+            return $zoneTest
+        } catch {
+            Write-Debug ($_.ToString())
+        }
+    }
+
+    return $null
+
+}
---
+++ docs/Plugins/Active24.md
@@ -0,0 +1,32 @@
+title: Active24
+
+# How To Use the Active24 DNS Plugin
+
+This plugin works against the [Active24](https://active24.com) DNS provider. It is assumed that you have already setup an account and created the DNS zone(s) you will be working against.
+
+
+## Setup
+
+First, [login](https://customer.active24.com/) to your account. Tokens are placed at  `Customer data edit` page, in `Tokens management`. There is need to type in your password again to manage the tokens.
+
+### Detailed walk through:
+* Using your login information log in to [Customer center](https://customer.active24.com/).
+* Click on the **user name** in the upper right corner or you may use direct link to the [API management](http://customer.active24.com/user/api).
+* You may **Enter** to the API management at the bottom of this page. 
+* Use the **Create a new token** button to setup new API token. 
+* The **Comment is not mandatory**, it simply helps to manage created tokens.
+* The **expiration date** is also optional.
+* In the **Access restrictions** can be specified the IP address of the device that can work with the token. If the device with a different IP address used functional token, it will not perform any requests - the requests cannot be authorized.
+* Once you fill everything you need, you can **Create a new token**. There is a **password confirmation** required.
+
+
+## Using the Plugin
+
+With your token value, you'll need to set the `A24Token` SecureString parameter.
+
+```powershell
+$pArgs = @{
+    A24Token = (Read-Host "Active24 Token" -AsSecureString)
+}
+New-PACertificate example.com -Plugin Active24 -PluginArgs $pArgs
+```
