rmbolger/Posh-ACME

commit ab4e161c3e118560e0632afc9fd4d0684cb20ec5

Fix plugin short name parsing for records that contain the zone name more than once (#586)
* fix regex based short name parsing in many plugins
* docs: fix short name parsing example in plugin dev guide

Posh-ACME/Plugins/DeSEC.ps1
@@ -236,11 +236,11 @@ function Find-DeSECRRset {
     )
 
     Write-Verbose "Attempting to find hosted zone for $RecordName"
-    if (!($domain = Find-DeSECZone $RecordName $AuthHeader)) {
+    if (!($zoneName = Find-DeSECZone $RecordName $AuthHeader)) {
         throw "Unable to find deSEC hosted zone for $RecordName"
     }
 
-    $subname = ($RecordName -ireplace [regex]::Escape($domain), [string]::Empty).TrimEnd('.')
+    $subname = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
 
     # .NET thinks all URLS are Windows filenames (no trailing dot)
     # replace trailing ... with escaped %2e%2e%2e

Posh-ACME/Plugins/Active24.ps1
@@ -31,7 +31,7 @@ function Add-DnsTxt {
 
     # get all the instances of the record
     try {
-        $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+        $recShort = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
         $recs = (Invoke-RestMethod "$apiRoot/dns/$zoneName/records/v1" @restParams -Method Get) | ? {$_.name -eq $recShort -and $_.type -eq "TXT"}
     } catch { throw }
 
@@ -104,7 +104,7 @@ function Remove-DnsTxt {
 
     # get all the instances of the record
     try {
-        $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+        $recShort = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
         $recs = (Invoke-RestMethod "$apiRoot/dns/$zoneName/records/v1" @restParams -Method Get) | ? {$_.name -eq $recShort -and $_.type -eq "TXT"}
     } catch { throw }
 

Posh-ACME/Plugins/Aliyun.ps1
@@ -27,7 +27,7 @@ function Add-DnsTxt {
     Write-Debug "Found zone $zoneName"
 
     # grab the relative portion of the fqdn
-    $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+    $recShort = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     if ($recShort -eq $RecordName) { $recShort = '@' }
 
     # query for an existing record
@@ -106,7 +106,7 @@ function Remove-DnsTxt {
     Write-Debug "Found zone $zoneName"
 
     # grab the relative portion of the fqdn
-    $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+    $recShort = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     if ($recShort -eq $RecordName) { $recShort = '@' }
 
     # query for an existing record

Posh-ACME/Plugins/All-Inkl.ps1
@@ -26,7 +26,7 @@ function Add-DnsTxt {
     $settingsObj = Get-KASDNSSettings $loginData $RecordName
 
     # remove zone from record name
-    $recNameWithoutZone = ($RecordName -ireplace [regex]::Escape($settingsObj.zone), [string]::Empty).TrimEnd('.')
+    $recNameWithoutZone = $RecordName -ireplace "\.?$([regex]::Escape($settingsObj.zone.TrimEnd('.')))$",''
 
     # search for existing DNS settings for the record
     $existingSettingsItem = Find-KASDNSSettingsItemInList $settingsObj.dnsSettings $recNameWithoutZone $TxtValue
@@ -116,7 +116,7 @@ function Remove-DnsTxt {
     $settingsObj = Get-KASDNSSettings $loginData $RecordName
 
     # remove zone from record name
-    $recNameWithoutZone = ($RecordName -ireplace [regex]::Escape($settingsObj.zone), [string]::Empty).TrimEnd('.')
+    $recNameWithoutZone = $RecordName -ireplace "\.?$([regex]::Escape($settingsObj.zone.TrimEnd('.')))$",''
 
     # search for existing DNS settings for the record
     $existingSettingsItem = Find-KASDNSSettingsItemInList $settingsObj.dnsSettings $recNameWithoutZone $TxtValue

Posh-ACME/Plugins/Aurora.ps1
@@ -5,37 +5,37 @@ function Add-DnsTxt {
     param(
         [Parameter(Mandatory, Position = 0)]
         [String]$RecordName,
-        
+
         [Parameter(Mandatory, Position = 1)]
         [String]$TxtValue,
-        
+
         [Parameter(Mandatory, Position = 2)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]$AuroraCredential = [System.Management.Automation.PSCredential]::Empty,
-        
+
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [String]$AuroraApi = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
     Write-Debug "convert the Credential to normal String values"
     $auroraAuthorization = @{ Api = $AuroraApi; Key = $AuroraCredential.UserName; Secret = $AuroraCredential.GetNetworkCredential().Password }
- 
+
     Write-Debug "Attempting to find hosted zone for $RecordName"
     try {
         $zone = Invoke-AuroraFindZone -RecordName $RecordName @auroraAuthorization
         $ZoneID = $zone.id
         $zoneName = $zone.name
     } catch { throw }
-    
+
     if ((-not $ZoneID) -or (-not $zoneName)) {
         throw "Unable to find Aurora hosted zone for $RecordName"
     }
-    
+
     Write-Debug "Separate the portion of the name that doesn't contain the zone name"
-    $recordPath = ($RecordName -ireplace [regex]::Escape($zoneName), [String]::Empty).TrimEnd('.')
+    $recordPath = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     Write-Debug "[recordPath:$recordPath][RecordName:$RecordName]"
 
     Write-Debug "Query the existing record(s)"
@@ -86,18 +86,18 @@ function Remove-DnsTxt {
     param(
         [Parameter(Mandatory, Position = 0)]
         [String]$RecordName,
-        
+
         [Parameter(Mandatory, Position = 1)]
         [String]$TxtValue,
-        
+
         [Parameter(Mandatory, Position = 2)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]$AuroraCredential = [System.Management.Automation.PSCredential]::Empty,
-        
+
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [String]$AuroraApi = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -110,13 +110,13 @@ function Remove-DnsTxt {
         $ZoneID = $zone.id
         $zoneName = $zone.name
     } catch { Write-Debug "Caught an error, $($_.Exception.Message)"; throw }
-    
+
     if ((-not $ZoneID) -or (-not $zoneName)) {
         throw "Unable to find Aurora hosted zone for $RecordName"
     }
-    
+
     Write-Debug "Separate the portion of the name that doesn't contain the zone name"
-    $recordPath = ($RecordName -ireplace [regex]::Escape($zoneName), [String]::Empty).TrimEnd('.')
+    $recordPath = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     Write-Debug "[recordPath:$recordPath][RecordName:$RecordName]"
 
     Write-Debug "Query the existing record(s)"
@@ -214,27 +214,27 @@ function Get-AuroraDNSAuthorizationHeader {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#>  
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [String]$Key,
-        
+
         [Parameter(Mandatory)]
         [String]$Secret,
-        
+
         [Parameter(Mandatory)]
         [String]$Method,
-        
+
         [Parameter(Mandatory)]
         [String]$Uri,
-        
+
         [Parameter()]
         [String]$ContentType = "application/json; charset=UTF-8",
-        
+
         [Parameter(DontShow)]
         [String]$TimeStamp = $((get-date).ToUniversalTime().ToString("yyyyMMddTHHmmssZ")),
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -246,7 +246,7 @@ function Get-AuroraDNSAuthorizationHeader {
     $AuthorizationString = '{0}:{1}' -f $Key, $SignatureB64
     $Authorization = [Text.Encoding]::UTF8.GetBytes($AuthorizationString)
     $AuthorizationB64 = [Convert]::ToBase64String($Authorization)
-    
+
     $headers = @{
         'X-AuroraDNS-Date' = $TimeStamp
         'Authorization'    = $('AuroraDNSv1 {0}' -f $AuthorizationB64)
@@ -296,25 +296,25 @@ function Invoke-AuroraAddRecord {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#> 
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [String]$Key,
-        
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [String]$Secret,
-        
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [GUID[]]$ZoneID,
-        
+
         [String]$Name = '',
-        
+
         [String]$Content = '',
-        
+
         [int]$TTL = 3600,
 
         [ValidateSet('A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'SRV', 'TXT', 'DS', 'PTR', 'SSHFP', 'TLSA')]
@@ -322,7 +322,7 @@ function Invoke-AuroraAddRecord {
 
         [Parameter()]
         [String]$Api = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -388,24 +388,24 @@ function Invoke-AuroraDeleteRecord {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#> 
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [string]$Key,
-        
+
         [Parameter(Mandatory)]
         [string]$Secret,
-        
+
         [Parameter(Mandatory)]
         [GUID[]]$ZoneID,
-        
+
         [Parameter(Mandatory)]
         [GUID[]]$RecordId,
-        
+
         [Parameter()]
         [String]$Api = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -418,7 +418,7 @@ function Invoke-AuroraDeleteRecord {
     $ApiUrl = 'https://{0}{1}' -f $Api, $Uri
     $AuthorizationHeader = Get-AuroraDNSAuthorizationHeader -Key $Key -Secret $Secret -Method $Method -Uri $Uri
     $restError = ''
-    
+
     Write-Debug "$Method URI: `"$ApiUrl`""
     try {
         $result = Invoke-RestMethod -Uri $ApiUrl -Headers $AuthorizationHeader -Method $Method -ErrorVariable restError @UseBasic
@@ -459,24 +459,24 @@ function Invoke-AuroraFindZone {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#>  
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory, Position = 0)]
         [String]$RecordName,
-        
+
         [Parameter(Mandatory, Position = 1)]
         [ValidateNotNullOrEmpty()]
         [String]$Key,
-        
+
         [Parameter(Mandatory, Position = 2)]
         [ValidateNotNullOrEmpty()]
         [String]$Secret,
-        
+
         [Parameter(Position = 3)]
         [ValidateNotNullOrEmpty()]
         [String]$Api = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -484,7 +484,7 @@ function Invoke-AuroraFindZone {
     try {
         [Object[]]$zones = Invoke-AuroraGetZones @auroraAuthorization
     } catch { Write-Debug "Caught an error, $($_.Exception.Message)"; throw }
-         
+
     Write-Debug "Search for the zone from longest to shortest set of FQDN pieces"
     $pieces = $RecordName.Split('.')
     for ($i = 0; $i -lt ($pieces.Count - 1); $i++) {
@@ -541,33 +541,33 @@ function Invoke-AuroraGetRecord {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#>  
+#>
     [CmdletBinding(DefaultParameterSetName = 'All')]
     param(
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [String]$Key,
-        
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [String]$Secret,
-        
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [GUID[]]$ZoneID,
 
         [Parameter(ParameterSetName = 'GUID', Mandatory)]
         [GUID[]]$RecordID,
-        
+
         [Parameter(ParameterSetName = 'Named')]
         [String]$RecordName,
-        
+
         [Parameter(ParameterSetName = 'Named')]
         [String]$Co,
-        
+
         [Parameter()]
         [String]$Api = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -602,7 +602,7 @@ function Invoke-AuroraGetRecord {
         } else {
             Throw ($OutError.errormsg)
         }
-    } 
+    }
     if ( ($result.Count -gt 0) -and ($null -ne $result[0].id) -and (-not [String]::IsNullOrEmpty($($result[0].id))) ) {
         Write-Output $result
     } else {
@@ -634,13 +634,13 @@ function Invoke-AuroraGetZones {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#>  
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [String]$Key,
-        
+
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [String]$Secret,
@@ -648,7 +648,7 @@ function Invoke-AuroraGetZones {
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [String]$Api = 'api.auroradns.eu',
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -717,25 +717,25 @@ function Invoke-AuroraSetRecord {
     Requires      : API Account => https://cp.pcextreme.nl/auroradns/users
 .LINK
     https://github.com/j81blog/Posh-AuroraDNS
-#> 
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [string]$Key,
-        
+
         [Parameter(Mandatory)]
         [string]$Secret,
-        
+
         [Parameter(Mandatory)]
         [GUID[]]$ZoneID,
-        
+
         [Parameter(Mandatory)]
         [GUID[]]$RecordId,
-        
+
         [String]$Name,
-        
+
         [String]$Content,
-        
+
         [int]$TTL = 3600,
 
         [ValidateSet('A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'SRV', 'TXT', 'DS', 'PTR', 'SSHFP', 'TLSA')]
@@ -745,7 +745,7 @@ function Invoke-AuroraSetRecord {
         [String]$Api = 'api.auroradns.eu',
 
         [Switch]$PassThru,
-        
+
         [Parameter(ValueFromRemainingArguments, DontShow)]
         $ExtraParams
     )
@@ -760,10 +760,10 @@ function Invoke-AuroraSetRecord {
     $restError = ''
 
     $Payload = @{ }
-    if ($PSBoundParameters.ContainsKey('Name')) { $Payload.Add('name', $Name) } 
-    if ($PSBoundParameters.ContainsKey('TTL')) { $Payload.Add('ttl', $TTL) } 
-    if ($PSBoundParameters.ContainsKey('Type')) { $Payload.Add('type', $Type) } 
-    if ($PSBoundParameters.ContainsKey('Content')) { $Payload.Add('content', $Content) } 
+    if ($PSBoundParameters.ContainsKey('Name')) { $Payload.Add('name', $Name) }
+    if ($PSBoundParameters.ContainsKey('TTL')) { $Payload.Add('ttl', $TTL) }
+    if ($PSBoundParameters.ContainsKey('Type')) { $Payload.Add('type', $Type) }
+    if ($PSBoundParameters.ContainsKey('Content')) { $Payload.Add('content', $Content) }
 
     $Body = $Payload | ConvertTo-Json
 

Posh-ACME/Plugins/AutoDNS.ps1
@@ -34,7 +34,7 @@ function Add-DnsTxt {
     # record to the zone now without checking to see if it already exists. During
     # testing, AutoDNS detects the duplicate record and just ignores it. So this is
     # probably fine.
-    $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+    $recShort = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     $updateBody = "<?xml version=`"1.0`" encoding=`"UTF-8`"?><request>$AuthBlock<task><code>0202001</code><default><rr_add><name>$recShort</name><ttl>600</ttl><type>TXT</type><value>$TxtValue</value></rr_add></default><zone><name>$zoneName</name><system_ns>$zoneNS</system_ns></zone></task></request>"
     try {
         Write-Verbose "Adding a TXT record for $RecordName with value $TxtValue"
@@ -121,7 +121,7 @@ function Remove-DnsTxt {
     # record from the zone now without checking to see if it already exists. During
     # testing, it doesn't seem to care if you try and remove a record that's already
     # gone. So this is fine.
-    $recShort = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+    $recShort = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     $updateBody = "<?xml version=`"1.0`" encoding=`"UTF-8`"?><request>$AuthBlock<task><code>0202001</code><default><rr_rem><name>$recShort</name><ttl>600</ttl><type>TXT</type><value>$TxtValue</value></rr_rem></default><zone><name>$zoneName</name><system_ns>$zoneNS</system_ns></zone></task></request>"
     try {
         Write-Verbose "Removing TXT record for $RecordName with value $TxtValue"

Posh-ACME/Plugins/Azure.ps1
@@ -751,12 +751,13 @@ function Get-AZTxtRecord {
     )
 
     # parse the zone name from the zone id and strip it from $RecordName
-    # to get the relativeRecordSetName
+    # to get the relative name
     $zoneName = $ZoneID.Substring($ZoneID.LastIndexOf('/')+1)
-    $relName = ($RecordName -ireplace [regex]::Escape($zoneName), [string]::Empty).TrimEnd('.')
+    $relName = $RecordName -ireplace "\.?$([regex]::Escape($zoneName.TrimEnd('.')))$",''
     if ($relName -eq [string]::Empty) {
         $relName = '@'
     }
+    Write-Debug "zoneName = $zoneName, relName = $relName"
 
     $recID = "$ZoneID/TXT/$($relName)"
 

Posh-ACME/Plugins/BlueCat.ps1
@@ -128,7 +128,7 @@ function Remove-DnsTxt {
     $proxy = Get-BlueCatWsdlProxy -Username $BlueCatUsername -Password $BlueCatPassword -Uri $BlueCatUri
     $view = Get-View -ConfigurationName $BlueCatConfig -ViewName $BlueCatView -BlueCatProxy $proxy
     $parentZone = Get-ParentZone -AbsoluteName $RecordName -ViewId $view.id -BlueCatProxy $proxy
-    $txtRecordName = ($RecordName -ireplace [regex]::Escape($parentZone.absoluteName), [string]::Empty).TrimEnd('.')
+    $txtRecordName = $RecordName -ireplace "\.?$([regex]::Escape($parentZone.absoluteName.TrimEnd('.')))$",''
     $txtRecords = $proxy.getEntitiesByName($parentZone.id, $txtRecordName, "TXTRecord", 0, [int16]::MaxValue)
     $txtRecords = $txtRecords | ForEach-Object { (ConvertPSObjectToHashtable -InputObject $_) + (StringToHashtable -String $_.properties) }
     $txtRecord = $txtRecords | Where-Object { $_.txt -eq $TxtValue }

commit 3ea65ea128db6b6db52315177457f07266ba549a

fix DeSEC bug caused by #586 (#598)

Posh-ACME/Plugins/DeSEC.ps1
@@ -245,16 +245,16 @@ function Find-DeSECRRset {
     # .NET thinks all URLS are Windows filenames (no trailing dot)
     # replace trailing ... with escaped %2e%2e%2e
     # https://stackoverflow.com/questions/856885/httpwebrequest-to-url-with-dot-at-the-end
-    $recordUri = "/domains/$($domain)/rrsets/$($subname)%2e%2e%2e/TXT/"
+    $recordUri = "/domains/$($zoneName)/rrsets/$($subname)%2e%2e%2e/TXT/"
     Write-Debug "$RecordName has URI: $recordUri"
 
     # get existing record
     try {
         $rrset = Invoke-deSEC $recordUri $AuthHeader
-        return $rrset, $recordUri, $domain, $subname
+        return $rrset, $recordUri, $zoneName, $subname
     } catch {
         if (404 -eq $_.Exception.Response.StatusCode) {
-            return $null, $null, $domain, $subname
+            return $null, $null, $zoneName, $subname
         }
         throw
     }